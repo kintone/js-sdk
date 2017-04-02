1a7bce7d8417b292bb5f0507ad3cc64b30829dbb
teppeis <teppeis@gmail.com>
2017-04-03 01:35:52 +0900

    test: add tests

--- packages/plugin-manifest-validator/index.js
+++ packages/plugin-manifest-validator/index.js
@@ -3,9 +3,27 @@
 const Ajv = require('ajv');
 const jsonSchema = require('./manifest-schema.json');
 
+/**
+ * @param {Object} json
+ * @return {{valid: boolean, errors: Array<!Object>}} errors is null if valid
+ */
 module.exports = function(json) {
-  const ajv = new Ajv();
+  const ajv = new Ajv({
+    unknownFormats: true,
+  });
   const validate = ajv.compile(jsonSchema);
   const valid = validate(json);
-  return {valid: valid, errors: validate.errors};
+  return {valid: valid, errors: transformErrors(validate.errors)};
 };
+
+/**
+ * @param {null|Array<Object>} errors
+ * @return {null|Array<Object>} shallow copy of the input or null
+ */
+function transformErrors(errors) {
+  if (!errors) {
+    return null;
+  }
+  // shallow copy
+  return errors.slice();
+}
--- packages/plugin-manifest-validator/test/index.js
+++ packages/plugin-manifest-validator/test/index.js
@@ -13,4 +13,83 @@ describe('validator', () => {
   it('minimal valid JSON', () => {
     assert.deepEqual(validator(require('./fixtures/minimal.json')), {valid: true, errors: null});
   });
+
+  it('missing property', () => {
+    assert.deepEqual(validator(require('./fixtures/no-version.json')), {
+      valid: false,
+      errors: [{
+        dataPath: '',
+        keyword: 'required',
+        message: "should have required property 'version'",
+        params: {
+          missingProperty: 'version',
+        },
+        schemaPath: '#/required',
+      }]
+    });
+  });
+
+  it('invalid type', () => {
+    assert.deepEqual(validator(require('./fixtures/version-is-string.json')), {
+      valid: false,
+      errors: [{
+        dataPath: '.version',
+        keyword: 'type',
+        message: 'should be integer',
+        params: {
+          type: 'integer',
+        },
+        schemaPath: '#/properties/version/type',
+      }]
+    });
+  });
+
+  it('integer is out of range', () => {
+    assert.deepEqual(validator(require('./fixtures/version-is-zero.json')), {
+      valid: false,
+      errors: [{
+        dataPath: '.version',
+        keyword: 'minimum',
+        message: 'should be >= 1',
+        params: {
+          comparison: '>=',
+          exclusive: false,
+          limit: 1,
+        },
+        schemaPath: '#/properties/version/minimum',
+      }]
+    });
+  });
+
+  it('invalid enum value', () => {
+    assert.deepEqual(validator(require('./fixtures/type-is-not-app.json')), {
+      valid: false,
+      errors: [{
+        dataPath: '.type',
+        keyword: 'enum',
+        message: 'should be equal to one of the allowed values',
+        params: {
+          allowedValues: [
+            'APP',
+          ],
+        },
+        schemaPath: '#/properties/type/enum',
+      }]
+    });
+  });
+
+  it('no English description', () => {
+    assert.deepEqual(validator(require('./fixtures/no-english-description.json')), {
+      valid: false,
+      errors: [{
+        dataPath: '.description',
+        keyword: 'required',
+        message: "should have required property 'en'",
+        params: {
+          missingProperty: 'en',
+        },
+        schemaPath: '#/properties/description/required',
+      }]
+    });
+  });
 });
