010edc9cfcfffa44cefc832f9e58f2b8fecfd6a5
teppeis <teppeis@gmail.com>
2017-04-02 16:42:42 +0900

    feat: validator

--- packages/plugin-manifest-validator/index.js
+++ packages/plugin-manifest-validator/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function() {
-  return 'foo';
+const Ajv = require('ajv');
+const jsonSchema = require('./manifest-schema.json');
+
+module.exports = function(json) {
+  const ajv = new Ajv();
+  const validate = ajv.compile(jsonSchema);
+  const valid = validate(json);
+  return {valid: valid, errors: validate.errors};
 };
--- packages/plugin-manifest-validator/test/index.js
+++ packages/plugin-manifest-validator/test/index.js
@@ -1,10 +1,16 @@
 'use strict';
 
+/* eslint-disable global-require */
+
 const assert = require('assert');
 const validator = require('../');
 
 describe('validator', () => {
   it('is a function', () => {
     assert(typeof validator === 'function');
+  });
+
+  it('minimal valid JSON', () => {
+    assert.deepEqual(validator(require('./fixtures/minimal.json')), {valid: true, errors: null});
   });
 });
