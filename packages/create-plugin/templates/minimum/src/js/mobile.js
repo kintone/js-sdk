6b193e96312352fd84abe1fbb08a42b7a383051a
Toru Kobayashi <koba0004@gmail.com>
2018-03-27 20:29:11 +0900

    Improve minimum template assets

--- packages/create-plugin/templates/minimum/src/js/mobile.js
+++ packages/create-plugin/templates/minimum/src/js/mobile.js
@@ -4,7 +4,11 @@ jQuery.noConflict();
     'use strict';
 
     kintone.events.on('mobile.app.record.index.show', function() {
-        kintone.mobile.app.getHeaderSpaceElement().textContent = 'Hello from kintone plugin! ID:' + PLUGIN_ID;
+        var spaceElement = kintone.mobile.app.getHeaderSpaceElement();
+        var el = document.createElement('div');
+        el.classList.add('plugin-space-element');
+        el.textContent = 'Hello from kintone plugin! ID:' + PLUGIN_ID;
+        spaceElement.appendChild(el);
     });
 
 })(jQuery, kintone.$PLUGIN_ID);
