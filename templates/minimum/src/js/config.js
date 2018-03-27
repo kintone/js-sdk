28cbe317e027b4ca83d803e9d771b531acc2ff5b
Toru Kobayashi <koba0004@gmail.com>
2018-03-27 15:14:34 +0900

    (subtree) Fix plugin format errors

--- templates/minimum/src/js/config.js
+++ templates/minimum/src/js/config.js
@@ -3,5 +3,5 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
-    alert('Hello from js/config.js ID:', PLUGIN_ID);
+    alert('Hello from js/config.js ID:' + PLUGIN_ID);
 })(jQuery, kintone.$PLUGIN_ID);
--- templates/minimum/src/js/desktop.js
+++ templates/minimum/src/js/desktop.js
@@ -3,5 +3,5 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
-    alert('Hello from js/desktop.js Plugin! ID:', PLUGIN_ID);
+    alert('Hello from js/desktop.js Plugin! ID:' + PLUGIN_ID);
 })(jQuery, kintone.$PLUGIN_ID);
