cd2356192d4bd7f98550a1c7fdd2ece4f5436203
Toru Kobayashi <koba0004@gmail.com>
2018-03-27 16:27:11 +0900

    Fix skeleton files

--- packages/create-plugin/templates/minimum/src/js/desktop.js
+++ packages/create-plugin/templates/minimum/src/js/desktop.js
@@ -3,5 +3,10 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
-    alert('Hello from js/desktop.js Plugin! ID:' + PLUGIN_ID);
+    kintone.events.on('app.record.index.show', function() {
+        var spaceElement = kintone.app.getHeaderSpaceElement();
+        spaceElement.classList.add('plugin-space-element');
+        spaceElement.textContent = 'Hello from kintone plugin! ID:' + PLUGIN_ID;
+    });
+
 })(jQuery, kintone.$PLUGIN_ID);
