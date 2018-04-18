2d33b8b4fe4aad3bd57d964db91a6b6202430922
Toru Kobayashi <koba0004@gmail.com>
2018-04-18 15:13:07 +0900

    test: add e2d testing

---
+++ packages/create-plugin/test/e2e.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import * as rimraf from 'rimraf';
+
+import { generatePlugin } from '../src/generator';
+
+describe('generator', () => {
+  let outputDir: string;
+  beforeEach(() => {
+    outputDir = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+    rimraf.sync(outputDir);
+  });
+  afterEach(() => {
+    rimraf.sync(outputDir);
+  });
+  it('should be able to create a plugin project based on the minimum template', function() {
+    // This timeout is for npm install
+    this.timeout(120000);
+    const manifest = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, 'manifest.json'), 'utf8')
+    );
+    generatePlugin(outputDir, manifest, 'ja');
+    assert.deepStrictEqual(
+      JSON.parse(
+        fs.readFileSync(path.resolve(outputDir, 'src', 'manifest.json'), 'utf8')
+      ),
+      manifest
+    );
+    assert.deepStrictEqual(
+      fs.readFileSync(
+        path.resolve(outputDir, 'src', 'js', 'desktop.js'),
+        'utf8'
+      ),
+      fs.readFileSync(
+        path.resolve(
+          __dirname,
+          '..',
+          'templates',
+          'minimum',
+          'src',
+          'js',
+          'desktop.js'
+        ),
+        'utf8'
+      )
+    );
+  });
+});
