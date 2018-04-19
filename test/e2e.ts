4a711110fb2989778c04bc4bffec2949eaaa49cc
Toru Kobayashi <koba0004@gmail.com>
2018-04-19 11:21:40 +0900

    (subtree) test: add test for npm run build (#46)

--- test/e2e.ts
+++ test/e2e.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as assert from 'assert';
+import { spawnSync } from 'child_process';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
@@ -24,6 +25,15 @@ describe('generator', () => {
       fs.readFileSync(path.resolve(__dirname, 'manifest.json'), 'utf8')
     );
     generatePlugin(outputDir, manifest, 'ja');
+
+    // test that `npm run build` doesn't fail
+    const buildResult = spawnSync('npm', ['run', 'build'], {
+      cwd: outputDir,
+      stdio: 'inherit',
+      shell: true
+    });
+    assert(buildResult.status === 0);
+
     assert.deepStrictEqual(
       JSON.parse(
         fs.readFileSync(path.resolve(outputDir, 'src', 'manifest.json'), 'utf8')
