5481b676b5aa8fc66c3d65a1a541630280a5c079
Toru Kobayashi <koba0004@gmail.com>
2018-06-07 11:31:30 +0900

    feat(template): use settings for the plugin (#124)
    * feat(template): use settings for the plugin
    
    * use required_params

--- packages/create-plugin/templates/minimum/src/js/desktop.js
+++ packages/create-plugin/templates/minimum/src/js/desktop.js
@@ -4,11 +4,21 @@ jQuery.noConflict();
     'use strict';
 
     kintone.events.on('app.record.index.show', function() {
+        var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
         var spaceElement = kintone.app.getHeaderSpaceElement();
-        var el = document.createElement('div');
-        el.classList.add('plugin-space-element');
-        el.textContent = 'Hello from kintone plugin! ID:' + PLUGIN_ID;
-        spaceElement.appendChild(el);
+        var fragment = document.createDocumentFragment();
+        var headingEl = document.createElement('h3');
+        var messageEl = document.createElement('p');
+
+        messageEl.classList.add('plugin-space-message');
+        messageEl.textContent = config.message;
+        headingEl.classList.add('plugin-space-heading');
+        headingEl.textContent = 'Hello kintone plugin!';
+
+        fragment.appendChild(headingEl);
+        fragment.appendChild(messageEl);
+        spaceElement.appendChild(fragment);
     });
 
 })(jQuery, kintone.$PLUGIN_ID);
