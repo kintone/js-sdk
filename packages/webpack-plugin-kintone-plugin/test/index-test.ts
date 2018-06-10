287ef5e57ea3dfe299a565af95e4c75f67da8806
Toru Kobayashi <koba0004@gmail.com>
2018-06-10 16:58:09 +0900

    refactor(test): apply tslint to test files (#123)

--- packages/webpack-plugin-kintone-plugin/test/index-test.ts
+++ packages/webpack-plugin-kintone-plugin/test/index-test.ts
@@ -1,48 +1,43 @@
-import * as AdmZip from 'adm-zip';
-import * as assert from 'assert';
-import { spawnSync } from 'child_process';
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import * as webpack from 'webpack';
+import * as AdmZip from "adm-zip";
+import * as assert from "assert";
+import { spawnSync } from "child_process";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import * as webpack from "webpack";
 // @ts-ignore
-import * as webpackConfig from './fixtures/sample/webpack.config';
+import * as webpackConfig from "./fixtures/sample/webpack.config";
 
-const pluginDir = path.resolve(__dirname, 'fixtures', 'sample');
-const pluginZipPath = path.resolve(pluginDir, 'dist', 'plugin.zip');
-const pluginJSPath = path.resolve(pluginDir, 'plugin', 'js', 'customize.js');
+const pluginDir = path.resolve(__dirname, "fixtures", "sample");
+const pluginZipPath = path.resolve(pluginDir, "dist", "plugin.zip");
+const pluginJSPath = path.resolve(pluginDir, "plugin", "js", "customize.js");
 const customNamePluginZipPath = path.resolve(
   pluginDir,
-  'dist',
-  'nfjiheanbocphdnoehhpddjmkhciokjb.sample.plugin.zip'
+  "dist",
+  "nfjiheanbocphdnoehhpddjmkhciokjb.sample.plugin.zip"
 );
 
-const runWebpack = (config = 'webpack.config.js') => {
-  const webpackCommand = `webpack${os.platform() === 'win32' ? '.cmd' : ''}`;
+const runWebpack = (config = "webpack.config.js") => {
+  const webpackCommand = `webpack${os.platform() === "win32" ? ".cmd" : ""}`;
   return spawnSync(
     webpackCommand,
-    [
-      '--config',
-      config,
-      '--mode',
-      'production'
-    ],
+    ["--config", config, "--mode", "production"],
     {
       cwd: pluginDir
     }
   );
-}
+};
 
 const verifyPluginZip = (zipPath: string) => {
   assert(fs.existsSync(zipPath));
   const zip = new AdmZip(zipPath);
   assert.deepStrictEqual(
     zip.getEntries().map((entry: AdmZip.IZipEntry) => entry.entryName),
-    ['contents.zip', 'PUBKEY', 'SIGNATURE']
+    ["contents.zip", "PUBKEY", "SIGNATURE"]
   );
 };
 
-describe('KintonePlugin', () => {
+describe("KintonePlugin", () => {
   afterEach(() => {
     // Cleanup the zip
     [pluginZipPath, customNamePluginZipPath, pluginJSPath].forEach(
@@ -55,13 +50,13 @@ describe('KintonePlugin', () => {
       }
     );
   });
-  it('should be able to create a plugin zip', () => {
+  it("should be able to create a plugin zip", () => {
     const rs = runWebpack();
     assert(rs.error == null, rs.error && rs.error.message);
     verifyPluginZip(pluginZipPath);
   });
-  it('should be able to customize the zip name', () => {
-    const rs = runWebpack('webpack.config.customize.name.js');
+  it("should be able to customize the zip name", () => {
+    const rs = runWebpack("webpack.config.customize.name.js");
     assert(rs.error == null, rs.error && rs.error.message);
     verifyPluginZip(customNamePluginZipPath);
   });
