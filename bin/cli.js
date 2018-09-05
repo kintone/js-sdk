ed6672b72eb32d215881b34bb35f7def79a5ad2a
miya-001 <masataka.miyahara0521@gmail.com>
2018-09-05 17:58:20 +0900

    (subtree) first commit

---
+++ bin/cli.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+const osLocale = require('os-locale');
+const meow = require('meow');
+const { run } = require('../dist/src/index');
+const { inquireParams } = require('../dist/src/params');
+const { getDefaultLang } = require('../dist/src/lang');
+const { getMessage } = require('../dist/src/messages');
+
+const {
+  HTTP_PROXY,
+  HTTPS_PROXY,
+  KINTONE_DOMAIN,
+  KINTONE_USERNAME,
+  KINTONE_PASSWORD
+} = process.env;
+
+const cli = meow(
+  `
+  Usage
+    $ kintone-customize-uploader <manifestFile>
+  Options
+    --domain Domain of your kintone
+    --username Login username
+    --password User's password
+    --proxy Proxy server
+    --watch Watch the changes of customize files and re-run
+    --waiting-dialog-ms A ms for waiting show a input dialog
+    --lang Using language (en or ja)
+    --guest-space-id Guest space ID for uploading files
+    You can set the values through environment variables
+    domain: KINTONE_DOMAIN
+    username: KINTONE_USERNAME
+    password: KINTONE_PASSWORD
+    proxy: HTTPS_PROXY or HTTP_PROXY
+`,
+  {
+    flags: {
+      domain: {
+        type: 'string',
+        default: KINTONE_DOMAIN
+      },
+      username: {
+        type: 'string',
+        default: KINTONE_USERNAME
+      },
+      password: {
+        type: 'string',
+        default: KINTONE_PASSWORD
+      },
+      proxy: {
+        type: 'string',
+        default: HTTPS_PROXY || HTTP_PROXY
+      },
+      watch: {
+        type: 'boolean',
+        default: false
+      },
+      waitingDialogMs: {
+        type: 'number',
+        default: 0
+      },
+      lang: {
+        type: 'string',
+        default: getDefaultLang(osLocale.sync())
+      },
+      guestSpaceId: {
+        type: 'number',
+        default: 0
+      },
+    }
+  }
+);
+
+const manifestFile = cli.input[0];
+const {
+  username,
+  password,
+  domain,
+  proxy,
+  watch,
+  waitingDialogMs,
+  lang,
+  guestSpaceId
+} = cli.flags;
+
+const options = proxy ? { watch, lang, proxy } : { watch, lang };
+if (guestSpaceId) {
+  options.guestSpaceId = guestSpaceId;
+}
+
+if (!manifestFile) {
+  console.error(getMessage(lang, 'E_requiredManifestFile'));
+  cli.showHelp();
+  process.exit(1);
+}
+
+const wait = ms => new Promise(r => setTimeout(r, ms));
+
+wait(waitingDialogMs)
+  .then(() => inquireParams({ username, password, domain, lang }))
+  .then(async({ username, password, domain }) => {
+    await run(domain, username, password, manifestFile, options);
+    if (!options.watch) {
+      process.exit();
+    }
+  });
---
+++ src/messages.ts
@@ -0,0 +1,86 @@
+import { Lang } from './lang';
+
+type LangMap = { [lang in Lang]: string };
+type MessageMap = { [key in keyof typeof messages]: LangMap };
+
+const messages = {
+  E_requiredManifestFile: {
+    en: 'Please specify manifest.json',
+    ja: 'マニフェストファイルを指定してください'
+  },
+  Q_Domain: {
+    en: 'Input your kintone\'s domain (example.cybozu.com):',
+    ja: 'kintoneのドメインを入力してください (example.cybozu.com):'
+  },
+  Q_UserName: {
+    en: 'Input your username:',
+    ja: 'ログイン名を入力してください:'
+  },
+  Q_Password: {
+    en: 'Input your password:',
+    ja: 'パスワードを入力してください:'
+  },
+  M_FileUploaded: {
+    en: 'JavaScript/CSS files have been uploaded!',
+    ja: 'JavaScript/CSS ファイルをアップロードしました!'
+  },
+  E_FileUploaded: {
+    en: 'Failed to upload JavaScript/CSS files',
+    ja: 'JavaScript/CSS ファイルのアップロードに失敗しました'
+  },
+  M_Uploaded: {
+    en: 'has been uploaded!',
+    ja: 'をアップロードしました!'
+  },
+  M_Updated: {
+    en: 'Customize setting has been updated!',
+    ja: 'JavaScript/CSS カスタマイズの設定を変更しました!'
+  },
+  E_Updated: {
+    en: 'Failed to update customize setting',
+    ja: 'JavaScript/CSS カスタマイズの設定の変更に失敗しました'
+  },
+  M_Deployed: {
+    en: 'Setting has been deployed!',
+    ja: '運用環境に反映しました!'
+  },
+  E_Deployed: {
+    en: 'Failed to deploy setting',
+    ja: '運用環境への反映に失敗しました'
+  },
+  M_Deploying: {
+    en: 'Wait for deploying completed...',
+    ja: '運用環境への反映の完了を待っています...'
+  },
+  M_Watching: {
+    en: 'Watcing for file changes...',
+    ja: 'ファイルの変更を監視しています...'
+  },
+  E_Retry: {
+    en: 'An error occured, retry',
+    ja: 'エラーが発生しました。リトライします'
+  },
+  E_Exit: {
+    en: 'An error occured, exit process. Please check if you passed proper arguments and manifest.json',
+    ja: 'エラーが発生しました。引数の値と、manifest.jsonに正しい値が入力されているか確認してください'
+  }
+};
+
+/**
+ * Returns a message for the passed lang and key
+ * @param lang
+ * @param key
+ */
+export function getMessage(lang: keyof LangMap, key: keyof MessageMap): string {
+  return messages[key][lang];
+}
+
+/**
+ * Returns a function bound lang to getMessage
+ * @param lang
+ */
+export function getBoundMessage(
+  lang: keyof LangMap
+): (key: keyof MessageMap) => string {
+  return getMessage.bind(null, lang);
+}
