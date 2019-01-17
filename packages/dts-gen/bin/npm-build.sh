dfd697dcf410ba38c74cdb0b128ad731d7685e2b
tomoya-yokota <tomoya-yokota@cybozu.co.jp>
2019-01-17 13:03:11 +0900

    Move source to to src directory / Introduct jest

--- packages/dts-gen/bin/npm-build.sh
+++ packages/dts-gen/bin/npm-build.sh
@@ -5,4 +5,4 @@ npm run tsc
 cat resources/shebang.txt dist/index.js > dist/index.js.tmp
 mv dist/index.js.tmp dist/index.js
 
-cp -v templates/*.njk dist/templates
+cp -v src/templates/*.njk dist/templates
