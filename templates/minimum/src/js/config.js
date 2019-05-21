1555545fdfebcf14c6e5e66660b80115a808eca0
Toru Kobayashi <koba0004@gmail.com>
2019-05-21 11:31:36 +0900

    (subtree) feat: use @cybozu/eslint-config instead of eslint-config-kintone (#314)

--- templates/minimum/src/js/config.js
+++ templates/minimum/src/js/config.js
@@ -1,24 +1,24 @@
 jQuery.noConflict();
 
 (function($, PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    var $form = $('.js-submit-settings');
-    var $message = $('.js-text-message');
+  var $form = $('.js-submit-settings');
+  var $message = $('.js-text-message');
+  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-    function getSettingsUrl() {
-        return '/k/admin/app/flow?app=' + kintone.app.getId();
-    }
+  function getSettingsUrl() {
+    return '/k/admin/app/flow?app=' + kintone.app.getId();
+  }
 
-    var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-    if (config.message) {
-        $message.val(config.message);
-    }
-    $form.on('submit', function(e) {
-        e.preventDefault();
-        kintone.plugin.app.setConfig({message: $message.val()}, function() {
-            alert('The plug-in settings have been saved. Please update the app!');
-            window.location.href = getSettingsUrl();
-        });
+  if (config.message) {
+    $message.val(config.message);
+  }
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    kintone.plugin.app.setConfig({message: $message.val()}, function() {
+      alert('The plug-in settings have been saved. Please update the app!');
+      window.location.href = getSettingsUrl();
     });
+  });
 })(jQuery, kintone.$PLUGIN_ID);
