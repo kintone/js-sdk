04b96337f4a841cf3965701122d04266847c6b54
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-08-20 11:35:21 +0900

    Fix `isUrlString` logic (#186)
    * fix: Fix `isUrlString`
    
    * Add `^` to regex
    
    Co-Authored-By: Hiroyuki ANAI <pirosikick@gmail.com>

--- packages/customize-uploader/src/util.ts
+++ packages/customize-uploader/src/util.ts
@@ -1,7 +1,5 @@
 export function isUrlString(str: string): boolean {
-  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/i.test(
-    str
-  );
+  return /^https?:\/\/.*/i.test(str);
 }
 
 export async function wait(ms: number): Promise<void> {
--- packages/customize-uploader/test/util-test.ts
+++ packages/customize-uploader/test/util-test.ts
@@ -5,9 +5,11 @@ describe("util", () => {
   describe("isUrlString", () => {
     it("should return true if the string is URL", () => {
       assert(isUrlString("https://example.com") === true);
+      assert(isUrlString("http://localhost:8000") === true);
     });
     it("should return false if the string is not URL", () => {
       assert(isUrlString("example.com") === false);
+      assert(isUrlString("js/desktop.js") === false);
     });
   });
 
