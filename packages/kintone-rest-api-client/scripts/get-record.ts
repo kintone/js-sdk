8f801ea754844e70de77cfb1ece22aa3bee8c90a
Toru Kobayashi <koba0004@gmail.com>
2019-10-21 15:53:24 +0900

    fix all ESLint errors

--- packages/kintone-rest-api-client/scripts/get-record.ts
+++ packages/kintone-rest-api-client/scripts/get-record.ts
@@ -1,14 +1,13 @@
-import { KintoneAPIClient } from '../src/index'
+import { KintoneAPIClient } from "../src/index";
 
 (async () => {
   const APP_ID = 8;
   const RECORD_ID = 3;
   const client = new KintoneAPIClient({
-    subdomain: process.env.KINTONE_SUBDOMAIN || '',
+    subdomain: process.env.KINTONE_SUBDOMAIN || "",
     auth: {
-      apiToken: process.env.KINTONE_API_TOKEN || ''
+      apiToken: process.env.KINTONE_API_TOKEN || ""
     }
   });
   console.log(await client.getRecord(APP_ID, RECORD_ID));
 })();
-
--- packages/kintone-rest-api-client/src/index.ts
+++ packages/kintone-rest-api-client/src/index.ts
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 type Auth = {
-  apiToken: string
-}
-type AppID = string | number
-type RecordID = string | number
+  apiToken: string;
+};
+type AppID = string | number;
+type RecordID = string | number;
 
 export class KintoneAPIClient {
-  private subdomain: string
-  private auth: Auth
-  constructor({subdomain, auth}: {subdomain: string, auth: Auth}) {
-    this.subdomain =  subdomain;
+  private subdomain: string;
+  private auth: Auth;
+  constructor({ subdomain, auth }: { subdomain: string; auth: Auth }) {
+    this.subdomain = subdomain;
     this.auth = auth;
   }
 
   public async getRecord(app: AppID, id: RecordID) {
-    const path = '/k/v1/record.json';
-    const headers = { 'X-Cybozu-API-Token': this.auth.apiToken};
+    const path = "/k/v1/record.json";
+    const headers = { "X-Cybozu-API-Token": this.auth.apiToken };
     const requestURL = `https://${this.subdomain}.cybozu.com${path}?app=${app}&id=${id}`;
     const { data } = await axios.get(requestURL, { headers });
     return data;
