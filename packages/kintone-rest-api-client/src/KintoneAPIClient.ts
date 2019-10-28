a9e5cabe2049bf4413bc8f93c2c76ff0a2c4615e
Toru Kobayashi <koba0004@gmail.com>
2019-10-29 01:59:59 +0900

    refactor: remove unnecessary httpClient property of KintoneAPIClient

--- packages/kintone-rest-api-client/src/KintoneAPIClient.ts
+++ packages/kintone-rest-api-client/src/KintoneAPIClient.ts
@@ -1,19 +1,18 @@
 import { RecordClient } from "./client/RecordClient";
-import { HttpClient, DefaultHttpClient } from "./http/";
+import { DefaultHttpClient } from "./http/";
 
 export type Auth = {
   apiToken: string;
 };
 
 export class KintoneAPIClient {
-  private httpClient: HttpClient;
   record: RecordClient;
 
   constructor({ subdomain, auth }: { subdomain: string; auth: Auth }) {
     const headers = { "X-Cybozu-API-Token": auth.apiToken };
     const url = `https://${subdomain}.cybozu.com/`;
-    this.httpClient = new DefaultHttpClient({ url, headers });
+    const httpClient = new DefaultHttpClient({ url, headers });
 
-    this.record = new RecordClient(this.httpClient);
+    this.record = new RecordClient(httpClient);
   }
 }
