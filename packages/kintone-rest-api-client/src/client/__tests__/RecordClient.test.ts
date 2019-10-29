7b11b7c619d517b195f4c71e05e267beaa33f485
Toru Kobayashi <koba0004@gmail.com>
2019-10-29 15:11:44 +0900

    test: implement addRecord, updateRecord, deleteRecords

--- packages/kintone-rest-api-client/src/client/__tests__/RecordClient.test.ts
+++ packages/kintone-rest-api-client/src/client/__tests__/RecordClient.test.ts
@@ -2,9 +2,82 @@ import { RecordClient } from "../RecordClient";
 import { MockClient } from "../../http/MockClient";
 
 describe("RecordClient", () => {
-  it("should be able to create an instance without any error", () => {
-    expect(() => {
-      new RecordClient(new MockClient());
-    }).not.toThrow();
+  let mockClient: MockClient;
+  let recordClient: RecordClient;
+  const APP_ID = 1;
+  const RECORD_ID = 2;
+  const record = {
+    Customer: {
+      value: "ABC Corporation"
+    }
+  };
+
+  beforeEach(() => {
+    mockClient = new MockClient();
+    recordClient = new RecordClient(mockClient);
+  });
+  describe("getRecord", () => {
+    beforeEach(() => {
+      recordClient.getRecord(APP_ID, RECORD_ID);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe("/k/v1/record.json");
+    });
+    it("should pass app and id as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual({
+        app: APP_ID,
+        id: RECORD_ID
+      });
+    });
+  });
+
+  describe("addRecord", () => {
+    beforeEach(() => {
+      recordClient.addRecord(APP_ID, record);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe("/k/v1/record.json");
+    });
+    it("should pass app and id as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual({ app: APP_ID, record });
+    });
+  });
+
+  describe("updateRecord", () => {
+    const params = {
+      id: RECORD_ID,
+      record,
+      revision: 5
+    };
+    beforeEach(() => {
+      recordClient.updateRecord(APP_ID, params);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe("/k/v1/record.json");
+    });
+    it("should pass app and id as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual({
+        app: APP_ID,
+        ...params
+      });
+    });
+  });
+
+  describe("deleteRecords", () => {
+    const ids = [10, 20, 30];
+    const revisions = [1, 2, 3];
+    beforeEach(() => {
+      recordClient.deleteRecords(APP_ID, ids, revisions);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe("/k/v1/records.json");
+    });
+    it("should pass app and id as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual({
+        app: APP_ID,
+        ids,
+        revisions
+      });
+    });
   });
 });
--- packages/kintone-rest-api-client/src/http/MockClient.ts
+++ packages/kintone-rest-api-client/src/http/MockClient.ts
@@ -1,22 +1,33 @@
 import { HttpClient } from "./HttpClientInterface";
 
 export class MockClient implements HttpClient {
+  logs: any[];
+  constructor() {
+    this.logs = [];
+  }
   public async get<T extends object>(path: string, params: object): Promise<T> {
+    this.logs.push({ path, params });
     return {} as T;
   }
   public async post<T extends object>(
     path: string,
     params: object
   ): Promise<T> {
+    this.logs.push({ path, params });
     return {} as T;
   }
   public async put<T extends object>(path: string, params: object): Promise<T> {
+    this.logs.push({ path, params });
     return {} as T;
   }
   public async delete<T extends object>(
     path: string,
     params: object
   ): Promise<T> {
+    this.logs.push({ path, params });
     return {} as T;
+  }
+  public getLogs(): any[] {
+    return this.logs;
   }
 }
