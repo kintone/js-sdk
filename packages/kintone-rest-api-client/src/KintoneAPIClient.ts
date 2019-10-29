641b00846fc93b8f9196b7165d1914fcd9d53bc9
shintaroNagata <cybozu.nagata@gmail.com>
2019-10-29 16:42:23 +0900

    create function for multiple auth header

--- packages/kintone-rest-api-client/src/KintoneAPIClient.ts
+++ packages/kintone-rest-api-client/src/KintoneAPIClient.ts
@@ -1,18 +1,43 @@
 import { RecordClient } from "./client/RecordClient";
 import { DefaultHttpClient } from "./http/";
+import { buildChildren } from "@babel/types";
 
-export type Auth = {
+export type Auth = ApiTokenAuth | PasswordAuth | SessionAuth;
+
+type ApiTokenAuth = {
   apiToken: string;
 };
+
+type PasswordAuth = {
+  username: string;
+  password: string;
+};
+
+type SessionAuth = {};
 
 export class KintoneAPIClient {
   record: RecordClient;
 
   constructor({ subdomain, auth }: { subdomain: string; auth: Auth }) {
-    const headers = { "X-Cybozu-API-Token": auth.apiToken };
+    const headers = this.buildAuthHeaders(auth);
     const url = `https://${subdomain}.cybozu.com/`;
     const httpClient = new DefaultHttpClient({ url, headers });
 
     this.record = new RecordClient(httpClient);
+  }
+
+  private buildAuthHeaders(auth: Auth) {
+    if ("username" in auth) {
+      const { username, password } = auth;
+      return {
+        "X-Cybozu-Authorization": Buffer.from(
+          `${username}:${password}`
+        ).toString("base64")
+      };
+    }
+    if ("apiToken" in auth) {
+      return { "X-Cybozu-API-Token": auth.apiToken };
+    }
+    return { "X-Requested-With": "XMLHttpRequest" };
   }
 }
