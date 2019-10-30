a85eda10d96cf416c13f3d397947e682278fa4ab
shintaroNagata <cybozu.nagata@gmail.com>
2019-10-30 14:57:38 +0900

    test: for client header

--- packages/kintone-rest-api-client/src/__tests__/KintoneAPIClient.test.ts
+++ packages/kintone-rest-api-client/src/__tests__/KintoneAPIClient.test.ts
@@ -1,12 +1,40 @@
 import { KintoneAPIClient } from "../KintoneAPIClient";
 
 describe("KintoneAPIClient", () => {
-  it("should be able to create an instance without any error", () => {
-    expect(() => {
-      new KintoneAPIClient({
-        subdomain: "example",
-        auth: { apiToken: "token" }
+  describe("constructor", () => {
+    describe("Header", () => {
+      const subdomain = "example";
+      it("ApiToken auth", () => {
+        const API_TOKEN = "ApiToken";
+        const auth = {
+          apiToken: API_TOKEN
+        };
+        const client = new KintoneAPIClient({ subdomain, auth });
+        expect(client.getHeaders()).toEqual({
+          "X-Cybozu-API-Token": API_TOKEN
+        });
       });
-    }).not.toThrow();
+      it("Password  auth", () => {
+        const USERNAME = "user";
+        const PASSWORD = "password";
+        const auth = {
+          username: USERNAME,
+          password: PASSWORD
+        };
+        const client = new KintoneAPIClient({ subdomain, auth });
+        expect(client.getHeaders()).toEqual({
+          "X-Cybozu-Authorization": Buffer.from(
+            `${USERNAME}:${PASSWORD}`
+          ).toString("base64")
+        });
+      });
+      it("Session auth", () => {
+        const auth = {};
+        const client = new KintoneAPIClient({ subdomain, auth });
+        expect(client.getHeaders()).toEqual({
+          "X-Requested-With": "XMLHttpRequest"
+        });
+      });
+    });
   });
 });
