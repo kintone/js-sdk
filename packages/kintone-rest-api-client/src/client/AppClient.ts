b60d3f6675f772b187afcbcc3333c04e1acd48b2
shintaroNagata <cybozu.nagata@gmail.com>
2019-11-11 16:04:07 +0900

    refactor: return type of getFormFields

--- packages/kintone-rest-api-client/src/client/AppClient.ts
+++ packages/kintone-rest-api-client/src/client/AppClient.ts
@@ -3,6 +3,10 @@ import { HttpClient } from "../http";
 type AppID = string | number;
 type Lang = "ja" | "en" | "zh" | "user" | "default";
 
+type Properties = {
+  [fieldCode: string]: any;
+};
+
 export class AppClient {
   private client: HttpClient;
 
@@ -10,7 +14,11 @@ export class AppClient {
     this.client = client;
   }
 
-  public async getFormFields(app: AppID, lang?: Lang, preview?: boolean) {
+  public async getFormFields<T extends Properties>(
+    app: AppID,
+    lang?: Lang,
+    preview?: boolean
+  ): Promise<{ properties: T; revision: string }> {
     const path = `/k/v1${preview ? "/preview" : ""}/app/form/fields.json`;
     return this.client.get(path, { app, lang });
   }
