86b39cb4307c90bb39f10d5ba7fdd5b156283747
Toru Kobayashi <koba0004@gmail.com>
2019-12-24 16:59:05 +0900

    feat: add umd build environment

---
+++ packages/rest-api-client/webpack.umd.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = (_, argv) => ({
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "umd"),
+    libraryTarget: "umd",
+    libraryExport: "KintoneAPIClient",
+    library: "KintoneRestApiClient",
+    filename: `KintoneRestApiClient${argv.mode === "production" ? ".min" : ""}.js`
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.json",
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  devtool: "inline-cheap-source-map"
+})
