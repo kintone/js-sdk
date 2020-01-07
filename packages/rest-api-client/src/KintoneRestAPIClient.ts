3436237ac37a0c9ad9da0f0b51709124742443c2
shintaroNagata <cybozu.nagata@gmail.com>
2020-01-07 15:10:14 +0900

    feat: enable RecordClient to receive guestSpaceId
    Co-authored-by: Toru Kobayashi <koba0004@gmail.com>

--- packages/rest-api-client/src/KintoneRestAPIClient.ts
+++ packages/rest-api-client/src/KintoneRestAPIClient.ts
@@ -51,10 +51,12 @@ export class KintoneRestAPIClient {
 
   constructor({
     host,
-    auth: partialAuth = {}
+    auth: partialAuth = {},
+    guestSpaceId
   }: {
     host: string;
     auth?: PartialAuth;
+    guestSpaceId?: number | string;
   }) {
     const auth = this.buildAuth(partialAuth);
     const params = this.buildParams(auth);
@@ -67,7 +69,7 @@ export class KintoneRestAPIClient {
     });
 
     this.bulkRequest_ = new BulkRequestClient(httpClient);
-    this.record = new RecordClient(httpClient);
+    this.record = new RecordClient(httpClient, guestSpaceId);
     this.app = new AppClient(httpClient);
     this.file = new FileClient(httpClient);
   }
