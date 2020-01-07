75b9b826fbcb4e6ece80ed9bcfb6f85750aadd13
chick-p <chick.p.1120@gmail.com>
2020-01-07 15:37:34 +0900

    feat: implement buildPath

---
+++ packages/rest-api-client/src/url.ts
@@ -0,0 +1,10 @@
+export const buildPath = (params: {
+  endpointName: string;
+  guestSpaceId?: number | string;
+  preview?: boolean;
+}) => {
+  const { endpointName, guestSpaceId, preview } = params;
+  const guestPath = guestSpaceId !== undefined ? `/guest/${guestSpaceId}` : "";
+  const previewPath = preview ? "/preview" : "";
+  return `/k${guestPath}/v1${previewPath}/${endpointName}.json`;
+};
