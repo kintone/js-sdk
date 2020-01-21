aa76409900a533669ac36faaa761fa211843e648
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-01-21 17:16:36 +0900

    feat: improve error message format

--- packages/rest-api-client/src/KintoneRestAPIError.ts
+++ packages/rest-api-client/src/KintoneRestAPIError.ts
@@ -18,7 +18,7 @@ export class KintoneRestAPIError extends Error {
     this.code = error.data.code;
     this.status = error.status;
     this.headers = error.headers;
-    this.message = `${error.data.message}\nStatus: ${error.status}\nCode: ${error.data.code}\nID: ${error.data.id}`;
+    this.message = `[${error.status}] [${error.data.code}] ${error.data.message} (${error.data.id}) `;
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Custom_Error_Types
     // Maintains proper stack trace for where our error was thrown (only available on V8)
