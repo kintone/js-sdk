1608c4e32c1511bd6704609418cc5aa0a9da5aa1
Toru Kobayashi <koba0004@gmail.com>
2020-02-03 16:15:49 +0900

    fix: remove unnecessary whitespace

--- packages/rest-api-client/src/KintoneRestAPIError.ts
+++ packages/rest-api-client/src/KintoneRestAPIError.ts
@@ -33,7 +33,7 @@ export class KintoneRestAPIError extends Error {
     this.errors = data.errors;
     this.status = error.status;
     this.headers = error.headers;
-    this.message = `[${error.status}] [${this.code}] ${this.message} (${this.id}) `;
+    this.message = `[${error.status}] [${this.code}] ${this.message} (${this.id})`;
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Custom_Error_Types
     // Maintains proper stack trace for where our error was thrown (only available on V8)
