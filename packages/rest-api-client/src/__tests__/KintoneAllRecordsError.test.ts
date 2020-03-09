3690f411481ae38c2dde29db948484e1efb5dea2
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-03-09 16:21:53 +0900

    test: add a test for message on KintoneAllRecordsError

--- packages/rest-api-client/src/__tests__/KintoneAllRecordsError.test.ts
+++ packages/rest-api-client/src/__tests__/KintoneAllRecordsError.test.ts
@@ -60,9 +60,49 @@ describe("KintoneAllRecordsError", () => {
       );
       expect(kintoneAllRecordsError.error).toStrictEqual(kintoneRestApiError);
     });
-    it.todo(
-      "should set a message that includes an error index if error.errros exisits"
-    );
-    it.todo("should set a message that includes the succeeded count");
+    it("should set a message that includes an error index if error.errors exists", () => {
+      expect(kintoneAllRecordsError.message).toBe(
+        `An error occurred at records[${kintoneAllRecordsError.errorIndex}]. ${
+          processedRecordsResult.length
+        }/${processedRecordsResult.length +
+          unprocessedRecords.length} records are processed successfully`
+      );
+    });
+    it("should set a message that includes the succeeded count", () => {
+      errorResponse = {
+        data: {
+          results: [
+            {},
+            {},
+            {
+              id: "some id",
+              code: "some code",
+              message: "some error message",
+              errors: {
+                unexpectedKey: {
+                  messages: ["key is missing"]
+                }
+              }
+            }
+          ]
+        },
+        status: 500,
+        headers: {
+          "X-Some-Header": "error"
+        }
+      };
+      kintoneRestApiError = new KintoneRestAPIError(errorResponse);
+      kintoneAllRecordsError = new KintoneAllRecordsError(
+        processedRecordsResult,
+        unprocessedRecords,
+        kintoneRestApiError,
+        chunkLength
+      );
+
+      expect(kintoneAllRecordsError.message).toBe(
+        `${processedRecordsResult.length}/${processedRecordsResult.length +
+          unprocessedRecords.length} records are processed successfully`
+      );
+    });
   });
 });
