4383b4cde32b420b5d704f92591ec6a7387e30eb
Toru Kobayashi <koba0004@gmail.com>
2020-03-09 16:02:09 +0900

    test: add a test for errorIndex property on KintoneAllRecordsError

---
+++ packages/rest-api-client/src/__tests__/KintoneAllRecordsError.test.ts
@@ -0,0 +1,62 @@
+import { KintoneAllRecordsError } from "../KintoneAllRecordsError";
+import { KintoneRestAPIError } from "../KintoneRestAPIError";
+import { ErrorResponse } from "../http/HttpClientInterface";
+
+describe("KintoneAllRecordsError", () => {
+  let kintoneAllRecordsError;
+  let kintoneRestApiError;
+  let errorResponse: ErrorResponse;
+  const processedRecordsResult = [{}, {}, {}, {}, {}];
+  const unprocessedRecords = [{}, {}, {}];
+  const chunkLength = 100;
+  // ref. errorResponse.data.results
+  const bulkRequestIndex = 2;
+  const errorParseResult = 5;
+  beforeEach(() => {
+    errorResponse = {
+      data: {
+        results: [
+          {},
+          {},
+          {
+            id: "some id",
+            code: "some code",
+            message: "some error message",
+            errors: {
+              [`records[${errorParseResult}].Customer`]: {
+                messages: ["key is missing"]
+              }
+            }
+          }
+        ]
+      },
+      status: 500,
+      headers: {
+        "X-Some-Header": "error"
+      }
+    };
+    kintoneRestApiError = new KintoneRestAPIError(errorResponse);
+    kintoneAllRecordsError = new KintoneAllRecordsError(
+      processedRecordsResult,
+      unprocessedRecords,
+      kintoneRestApiError,
+      chunkLength
+    );
+  });
+  describe("constructor", () => {
+    it("should set errorIndex from an error", () => {
+      expect(kintoneAllRecordsError.errorIndex).toBe(
+        processedRecordsResult.length +
+          bulkRequestIndex * chunkLength +
+          errorParseResult
+      );
+    });
+    it.todo(
+      "should set processedRecordsResult, unprocessedRecords, and error properties"
+    );
+    it.todo(
+      "should set a message that includes an error index if error.errros exisits"
+    );
+    it.todo("should set a message that includes the succeeded count");
+  });
+});
