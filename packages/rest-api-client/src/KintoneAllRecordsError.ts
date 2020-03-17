6bd9ccec6e66b742ecc2cb4a68792738846c926d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-03-17 17:30:10 +0900

    fix: set the prototype explicitly in KintoneRestAPIError and KintoneAllRecordsError, too

--- packages/rest-api-client/src/KintoneAllRecordsError.ts
+++ packages/rest-api-client/src/KintoneAllRecordsError.ts
@@ -73,5 +73,9 @@ export class KintoneAllRecordsError extends Error {
     this.error = error;
     this.errorIndex = errorIndex;
     this.message = message;
+
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, KintoneAllRecordsError.prototype);
   }
 }
