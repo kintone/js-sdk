1c24010fd1626ad8a46acc7368b2fdfde86af321
shintaroNagata <cybozu.nagata@gmail.com>
2020-04-15 15:33:55 +0900

    refactor: replace type definitions for RecordClient & AppClient

---
+++ packages/rest-api-client/src/types/record.ts
@@ -0,0 +1,26 @@
+export type Record = {
+  [fieldCode: string]: any;
+};
+
+export type UpdateKey = {
+  field: string;
+  value: string | number;
+};
+
+export type Mention = {
+  code: string;
+  type: "USER" | "GROUP" | "ORGANIZATION";
+};
+
+export type Comment = {
+  id: string;
+  text: string;
+  createdAt: string;
+  creator: {
+    code: string;
+    name: string;
+  };
+  mentions: Mention[];
+};
+
+export type CommentID = string | number;
