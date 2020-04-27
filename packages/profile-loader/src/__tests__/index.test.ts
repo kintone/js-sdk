801ad24d5d4ea5114826d55ad898e5f730e86ff9
Toru Kobayashi <koba0004@gmail.com>
2020-04-27 14:18:50 +0900

    feat(profile-loader): be able to parse environment variable and config file

--- packages/profile-loader/src/__tests__/index.test.ts
+++ packages/profile-loader/src/__tests__/index.test.ts
@@ -1,14 +1,64 @@
+import path from "path";
 import { loadProfile } from "../";
 
 describe("index", () => {
+  let originalUsername: string | undefined;
+  let originalPassword: string | undefined;
+  let originalBaseUrl: string | undefined;
+  let originalApiToken: string | undefined;
+  let originalOAuthToken: string | undefined;
+  beforeEach(() => {
+    originalUsername = process.env.KINTONE_USERNAME;
+    originalPassword = process.env.KINTONE_PASSWORD;
+    originalBaseUrl = process.env.KINTONE_BASE_URL;
+    originalApiToken = process.env.KINTONE_API_TOKEN;
+    originalOAuthToken = process.env.KINTONE_OAUTH_TOKEN;
+    delete process.env.KINTONE_USERNAME;
+    delete process.env.KINTONE_PASSWORD;
+    delete process.env.KINTONE_BASE_URL;
+    delete process.env.KINTONE_API_TOKEN;
+    delete process.env.KINTONE_OAUTH_TOKEN;
+  });
+  afterEach(() => {
+    process.env.KINTONE_USERNAME = originalUsername;
+    process.env.KINTONE_PASSWORD = originalPassword;
+    process.env.KINTONE_BASE_URL = originalBaseUrl;
+    process.env.KINTONE_API_TOKEN = originalApiToken;
+    process.env.KINTONE_OAUTH_TOKEN = originalOAuthToken;
+  });
   describe("loadProfile", () => {
     it("should return a profile", () => {
       expect(loadProfile()).toEqual({
-        username: "",
-        password: "",
-        baseUrl: "",
-        apiToken: "",
-        oAuthToken: "",
+        username: null,
+        password: null,
+        baseUrl: null,
+        apiToken: null,
+        oAuthToken: null,
+      });
+    });
+    it("should be able to load setting from environment variable", () => {
+      process.env.KINTONE_USERNAME = "bob";
+      process.env.KINTONE_PASSWORD = "password";
+      process.env.KINTONE_BASE_URL = "https://example.kintone.com";
+      process.env.KINTONE_API_TOKEN = "api_token";
+      process.env.KINTONE_OAUTH_TOKEN = "oauth_token";
+      expect(loadProfile()).toEqual({
+        username: "bob",
+        password: "password",
+        baseUrl: "https://example.kintone.com",
+        apiToken: "api_token",
+        oAuthToken: "oauth_token",
+      });
+    });
+    it("should be able to load setting from environment variable", () => {
+      expect(
+        loadProfile(path.resolve(__dirname, "fixtures", "config"))
+      ).toEqual({
+        username: "jim",
+        password: "foo",
+        baseUrl: "https://foo.kintone.com",
+        apiToken: "api_token_config",
+        oAuthToken: "oauth_token_config",
       });
     });
   });
--- packages/profile-loader/src/index.ts
+++ packages/profile-loader/src/index.ts
@@ -1,19 +1,47 @@
-console.log("Hello");
+import toml from "toml";
+import fs from "fs";
+import path from "path";
 
 type Profile = {
-  username: string;
-  password: string;
-  baseUrl: string;
-  apiToken: string;
-  oAuthToken: string;
+  username: string | null;
+  password: string | null;
+  baseUrl: string | null;
+  apiToken: string | null;
+  oAuthToken: string | null;
 };
 
-export const loadProfile = (): Profile => {
+export const loadProfile = (configFilePath = getConfigFilePath()): Profile => {
   return {
-    username: "",
-    password: "",
-    baseUrl: "",
-    apiToken: "",
-    oAuthToken: "",
+    username: null,
+    password: null,
+    baseUrl: null,
+    apiToken: null,
+    oAuthToken: null,
+    ...loadEnvironmentVariable(),
+    ...loadConfig(configFilePath),
   };
+};
+
+const loadEnvironmentVariable = (): Partial<Profile> => ({
+  username: process.env.KINTONE_USERNAME || null,
+  password: process.env.KINTONE_PASSWORD || null,
+  baseUrl: process.env.KINTONE_BASE_URL || null,
+  apiToken: process.env.KINTONE_API_TOKEN || null,
+  oAuthToken: process.env.KINTONE_OAUTH_TOKEN || null,
+});
+
+const loadConfig = (filePath: string | null): Partial<Profile> => {
+  if (!filePath) {
+    return {};
+  }
+  const tomlString = fs.readFileSync(filePath).toString();
+  return toml.parse(tomlString);
+};
+
+const getConfigFilePath = (): string | null => {
+  const filePath = path.resolve(process.cwd(), ".kintone", "config");
+  if (fs.existsSync(filePath)) {
+    return filePath;
+  }
+  return null;
 };
