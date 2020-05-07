c1fbfae4d04ca5b91c9404853e2ffb7761d5b31d
shintaroNagata <cybozu.nagata@gmail.com>
2020-05-07 18:58:37 +0900

    feat: add type definitions for field layout

--- packages/rest-api-client/src/client/types/app/form.ts
+++ packages/rest-api-client/src/client/types/app/form.ts
@@ -4,7 +4,4 @@ export type Properties = {
   [fieldCode: string]: FieldProperty;
 };
 
-// TODO: Make this type more specific
-export type Layout = Array<{
-  [k: string]: any;
-}>;
+export { Layout } from "./layout";
