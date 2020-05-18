24e7c42089de2b54b57bcd2d075a5b847d19e155
Toru Kobayashi <koba0004@gmail.com>
2020-05-18 16:45:34 +0900

    test: fix an ESLint error caused by type-fest

--- packages/rest-api-client/.eslintrc.js
+++ packages/rest-api-client/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: "@cybozu/eslint-config/presets/node-typescript-prettier"
+  extends: "@cybozu/eslint-config/presets/node-typescript-prettier",
+  "rules": {
+    "node/no-missing-import": ["error", {
+      "allowModules": ["type-fest"]
+    }]
+  }
 };
