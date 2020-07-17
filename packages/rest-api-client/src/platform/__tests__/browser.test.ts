5d6d0b890d02b84d5457163226b5bb20d393c7c7
chick-p <chick.p.1120@gmail.com>
2020-07-17 09:34:02 +0900

    refactor(test): extract token to a variable

--- packages/rest-api-client/src/platform/__tests__/browser.test.ts
+++ packages/rest-api-client/src/platform/__tests__/browser.test.ts
@@ -12,22 +12,24 @@ describe("getRequestToken()", () => {
     global.kintone = originalKintone;
   });
   it("should get a request token in kintone", async () => {
+    const TOKEN = "dummy request token from kintone";
     global.kintone = {
-      getRequestToken: () => "dummy request token from kintone",
+      getRequestToken: () => TOKEN,
     };
     const requestToken = await getRequestToken();
-    expect(requestToken).toBe("dummy request token from kintone");
+    expect(requestToken).toBe(TOKEN);
   });
   it("should get a request token in garoon", async () => {
+    const TOKEN = "dummy request token from garoon";
     global.garoon = {
       connect: {
         kintone: {
-          getRequestToken: async () => "dummy request token from garoon",
+          getRequestToken: async () => TOKEN,
         },
       },
     };
     const requestToken = await getRequestToken();
-    expect(requestToken).toBe("dummy request token from garoon");
+    expect(requestToken).toBe(TOKEN);
   });
   it("should throw an error in other service", async () => {
     await expect(getRequestToken()).rejects.toThrow(
