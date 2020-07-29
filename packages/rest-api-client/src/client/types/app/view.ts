af8a04d30aa05240bc0db60988b12d2830508a98
shintaroNagata <cybozu.nagata@gmail.com>
2020-07-29 11:07:45 +0900

    refactor: make type definitions for views readable

--- packages/rest-api-client/src/client/types/app/view.ts
+++ packages/rest-api-client/src/client/types/app/view.ts
@@ -1,50 +1,61 @@
-import { Appearance, ConditionalStrict, ConditionalExist } from "./utilityType";
+import { Appearance } from "./utilityType";
 
-type ViewBase<T extends Appearance> = {
-  index: T extends "response"
-    ? string
-    : T extends "parameter"
-    ? string | number
-    : never;
-} & ConditionalExist<T, "response", { builtinType?: "ASSIGNEE"; id: string }> &
-  ConditionalStrict<
-    T,
-    "response",
-    { name: string; filterCond: string; sort: string }
-  >;
-
-type ListView<T extends Appearance> = ViewBase<T> & {
-  type: "LIST";
-} & ConditionalStrict<
-    T,
-    "response",
-    {
-      fields: string[];
+type ViewBase<T extends Appearance> = T extends "response"
+  ? {
+      index: string;
+      builtinType?: "ASSIGNEE";
+      id: string;
+      name: string;
+      filterCond: string;
+      sort: string;
     }
-  >;
+  : {
+      index: string | number;
+      name?: string;
+      filterCond?: string;
+      sort?: string;
+    };
 
-type CalendarView<T extends Appearance> = ViewBase<T> & {
-  type: "CALENDAR";
-} & ConditionalStrict<
-    T,
-    "response",
-    {
-      date: string;
-      title: string;
-    }
-  >;
+type ListView<T extends Appearance> = ViewBase<T> &
+  (T extends "response"
+    ? {
+        type: "LIST";
+        fields: string[];
+      }
+    : {
+        type: "LIST";
+        fields?: string[];
+      });
 
-type CustomView<T extends Appearance> = ViewBase<T> & {
-  type: "CUSTOM";
-} & ConditionalStrict<
-    T,
-    "response",
-    {
-      html: string;
-      pager: boolean;
-      device: "DESKTOP" | "ANY";
-    }
-  >;
+type CalendarView<T extends Appearance> = ViewBase<T> &
+  (T extends "response"
+    ? {
+        type: "CALENDAR";
+        date: string;
+        title: string;
+      }
+    : {
+        type: "CALENDAR";
+        date?: string;
+        title?: string;
+      });
+
+type Device = "DESKTOP" | "ANY";
+
+type CustomView<T extends Appearance> = ViewBase<T> &
+  (T extends "response"
+    ? {
+        type: "CUSTOM";
+        html: string;
+        pager: boolean;
+        device: Device;
+      }
+    : {
+        type: "CUSTOM";
+        html?: string;
+        pager?: boolean;
+        device?: Device;
+      });
 
 export type View<T extends Appearance> =
   | ListView<T>
