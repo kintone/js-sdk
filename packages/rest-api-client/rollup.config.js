3f061880864bc28f3ebbc80bae77698cb231bb1b
shisama <shisama07@gmail.com>
2020-08-06 22:05:24 +0900

    fix: umd bundle with babel

--- packages/rest-api-client/rollup.config.js
+++ packages/rest-api-client/rollup.config.js
@@ -1,32 +1,38 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+// import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 import replace from "rollup-plugin-replace";
 import pkgJson from "./package.json";
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import babel from "@rollup/plugin-babel";
+
+const extensions = [".ts", ".js"];
 
 export default (argv) => {
   const isProd = argv.mode === "production";
   const plugins = [
-    typescript({
-      declaration: false,
+    // typescript({
+    //   declaration: false,
+    // }),
+    babel({
+      babelHelpers: "bundled",
+      extensions,
+      include: ["src/**/*"],
     }),
     resolve({
       browser: true,
       preferBuiltins: false,
     }),
-    commonjs({ extensions: [".ts", ".js"] }),
+    commonjs({ extensions }),
     json(),
     replace({
       PACKAGE_VERSION: JSON.stringify(pkgJson.version),
     }),
     globals(),
     builtins(),
-    nodePolyfills(),
   ];
   if (isProd) {
     plugins.push(terser());
@@ -37,7 +43,7 @@ export default (argv) => {
       file: `./umd/KintoneRestAPIClient${isProd ? ".min" : ""}.js`,
       format: "umd",
       name: "KintoneRestAPIClient",
-      compact: true,
+      sourcemap: isProd ? false : "inline",
     },
     plugins,
   };
