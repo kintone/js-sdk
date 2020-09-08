ae3df39eaa4223c4f1805f0e9dac8d815562f66b
Masashi Hirano <shisama07@gmail.com>
2020-09-08 18:33:18 +0900

    refactor: replace request-promise with @kitnone/rest-api-client (#360)
    * refactor: replace request-promise to @kitnone/rest-api-client in customize-uploader
    
    * fix: add mock functions into MockKintoneApiClient
    
    * refactor: change unnecessary array to object
    
    * test: fix MockKintoneApiClient interface

--- packages/customize-uploader/src/KintoneApiClient.ts
+++ packages/customize-uploader/src/KintoneApiClient.ts
@@ -1,36 +1,21 @@
-import fs from "fs";
-import request from "request-promise";
+import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 import { isUrlString, wait } from "./util";
-
-interface RequestOption {
-  method: string;
-  url: string;
-  headers: {
-    [propName: string]: any;
-  };
-  body: object | string | null;
-  formData?: object;
-  proxy?: string;
-  tunnel?: boolean;
-  resolveWithFullResponse: boolean;
-}
 
 export interface Option {
   proxy: string;
   guestSpaceId: number;
 }
 
-export interface RequestParams {
-  method: "GET" | "POST" | "PUT" | "DELETE";
-  path: string;
-  body: object;
-  contentType?: string;
-}
+export type UpdateAppCustomizeParameter = {
+  app: string | number;
+  scope?: "ALL" | "ADMIN" | "NONE" | undefined;
+  desktop?: { [key: string]: unknown };
+  mobile?: { [key: string]: unknown };
+  revision?: string | number;
+};
 
 export default class KintoneApiClient {
-  private auth: string;
-  private basicAuth: string | null;
-  private kintoneUrl: string;
+  private restApiClient: KintoneRestAPIClient;
   public constructor(
     username: string,
     password: string,
@@ -39,33 +24,34 @@ export default class KintoneApiClient {
     domain: string,
     public options: Option
   ) {
-    this.auth = this.getBase64EncodedCredentials(username, password);
-    this.basicAuth =
-      basicAuthUsername && basicAuthPassword
-        ? this.getBasicAuthorization(basicAuthUsername, basicAuthPassword)
-        : null;
-    this.kintoneUrl =
+    const kintoneUrl =
       domain.indexOf("https://") > -1 ? domain : `https://${domain}`;
-  }
-
-  public uploadFile(filePath: string, contentType: string) {
-    return this.sendRequest({
-      method: "POST",
-      path: "/k/v1/file.json",
-      body: {
-        file: {
-          value: fs.createReadStream(filePath, "utf8"),
-          options: { contentType },
-        },
+    let basicAuth;
+    if (basicAuthUsername && basicAuthPassword) {
+      basicAuth = {
+        username: basicAuthUsername,
+        password: basicAuthPassword,
+      };
+    }
+    this.restApiClient = new KintoneRestAPIClient({
+      baseUrl: kintoneUrl,
+      auth: { username, password },
+      basicAuth,
+      featureFlags: {
+        enableAbortSearchError: false,
       },
-      contentType: "multipart/form-data",
     });
   }
 
-  public async prepareCustomizeFile(
-    fileOrUrl: string,
-    contentType: string
-  ): Promise<any> {
+  public uploadFile(filePath: string) {
+    return this.restApiClient.file.uploadFile({
+      file: {
+        path: filePath,
+      },
+    });
+  }
+
+  public async prepareCustomizeFile(fileOrUrl: string): Promise<any> {
     const isUrl = isUrlString(fileOrUrl);
     if (isUrl) {
       return {
@@ -73,7 +59,7 @@ export default class KintoneApiClient {
         url: fileOrUrl,
       };
     }
-    const { fileKey } = await this.uploadFile(fileOrUrl, contentType);
+    const { fileKey } = await this.uploadFile(fileOrUrl);
     return {
       type: "FILE",
       file: {
@@ -82,31 +68,23 @@ export default class KintoneApiClient {
     };
   }
 
-  public updateCustomizeSetting(setting: any) {
-    return this.sendRequest({
-      method: "PUT",
-      path: "/k/v1/preview/app/customize.json",
-      body: setting,
-    });
+  public updateCustomizeSetting(params: UpdateAppCustomizeParameter) {
+    return this.restApiClient.app.updateAppCustomize(params);
   }
 
   public deploySetting(appId: string) {
-    return this.sendRequest({
-      method: "POST",
-      path: "/k/v1/preview/app/deploy.json",
-      body: { apps: [{ app: appId }] },
+    return this.restApiClient.app.deployApp({
+      apps: [{ app: appId }],
     });
   }
 
   public async waitFinishingDeploy(appId: string, callback: () => void) {
     let deployed = false;
     while (!deployed) {
-      const resp = await this.sendRequest({
-        method: "GET",
-        path: "/k/v1/preview/app/deploy.json",
-        body: { apps: [appId] },
+      const resp = await this.restApiClient.app.getDeployStatus({
+        apps: [appId],
       });
-      const successedApps: [any] = resp.apps;
+      const successedApps = resp.apps;
       const successedAppsLength = successedApps.filter((r) => {
         return r.status === "SUCCESS";
       }).length;
@@ -119,83 +97,16 @@ export default class KintoneApiClient {
     deployed = true;
   }
 
-  public async downloadFile(fileKey: string) {
-    return this.sendRequest({
-      method: "GET",
-      path: "/k/v1/file.json",
-      body: { fileKey },
+  public downloadFile(fileKey: string) {
+    return this.restApiClient.file.downloadFile({
+      fileKey,
     });
   }
 
-  public async getAppCustomize(appId: string) {
-    return this.sendRequest({
-      method: "GET",
-      path: "/k/v1/app/customize.json",
-      body: { app: appId },
+  public getAppCustomize(appId: string) {
+    return this.restApiClient.app.getAppCustomize({
+      app: appId,
     });
-  }
-
-  public async sendRequest(params: RequestParams) {
-    const requestOptions = this.buildRequestOptions(params);
-    try {
-      return request(requestOptions).then((response) => {
-        if (response.statusCode !== 200) {
-          throw new Error(
-            `Failed to Request(StatusCode:${response.statusCode}`
-          );
-        }
-        const contentType = response.headers["content-type"];
-        if (contentType && contentType.startsWith("application/json")) {
-          return JSON.parse(response.body);
-        }
-        return response.body;
-      });
-    } catch (e) {
-      if (e.statusCode === 520) {
-        const responseBody = JSON.parse(e.response.body);
-        // We assume that CB_WA01 is an authentication error
-        if (responseBody.code === "CB_WA01") {
-          throw new AuthenticationError(responseBody.message);
-        }
-      }
-      throw new Error("Unexpected Error:" + e.toString());
-    }
-  }
-
-  private buildRequestOptions({
-    method,
-    path,
-    body,
-    contentType,
-  }: RequestParams): RequestOption {
-    const isFormData = contentType && contentType === "multipart/form-data";
-    const requestOptions: RequestOption = Object.assign(
-      {
-        method,
-        url: this.options.guestSpaceId
-          ? this.kintoneUrl +
-            `/k/guest/${this.options.guestSpaceId}` +
-            path.slice(2)
-          : this.kintoneUrl + path,
-        headers: {
-          "X-Cybozu-Authorization": this.auth,
-          "Content-Type": contentType || "application/json",
-        },
-        resolveWithFullResponse: true,
-      },
-      isFormData
-        ? { formData: body, body: null }
-        : { body: JSON.stringify(body) }
-    );
-    if (this.basicAuth) {
-      requestOptions.headers.Authorization = this.basicAuth;
-    }
-    if (this.options.proxy) {
-      requestOptions.proxy = this.options.proxy;
-      requestOptions.tunnel = true;
-    }
-    requestOptions.resolveWithFullResponse = true;
-    return requestOptions;
   }
 
   private getBase64EncodedCredentials(
