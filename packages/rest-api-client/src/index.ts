f703df112588ae113ca1659152d056280b4e7d11
Hodaka Kasama <hodaka-kasama@cybozu.co.jp>
2020-09-09 10:21:52 +0900

    export Kintone Fields from package root

--- packages/rest-api-client/src/index.ts
+++ packages/rest-api-client/src/index.ts
@@ -1,6 +1,14 @@
 import { injectPlatformDeps } from "./platform/";
 import * as nodeDeps from "./platform/node";
+import * as KintoneField from "./KintoneFields/types/field";
+import * as KintoneLayout from "./KintoneFields/types/layout";
+import * as KintoneProperty from "./KintoneFields/types/property";
 
 injectPlatformDeps(nodeDeps);
 
 export { KintoneRestAPIClient } from "./KintoneRestAPIClient";
+export const KintoneFields = {
+  ...KintoneField,
+  ...KintoneLayout,
+  ...KintoneProperty,
+};
