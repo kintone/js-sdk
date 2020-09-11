464b587b6a54b0d707a792d0264d0e9724a8e821
Toru Kobayashi <koba0004@gmail.com>
2020-09-11 15:15:57 +0900

    test: add a test to get records

--- src/commands/__tests__/export.test.ts
+++ src/commands/__tests__/export.test.ts
@@ -10,13 +10,32 @@ describe("export", () => {
     });
   });
   it("should not be failed", () => {
-    apiClient.record.getRecords = jest
-      .fn()
-      .mockResolvedValue({ records: [{}] });
+    apiClient.record.getAllRecords = jest.fn().mockResolvedValue([{}]);
     return expect(
       exportRecords(apiClient, { app: "1", attachmentDir: "" })
     ).resolves.not.toThrow();
   });
-  it.todo("can get records");
+  it("can get records", async () => {
+    const records = [
+      {
+        fieldCode: {
+          type: "SINGLE_LINE_TEXT",
+          value: "value1",
+        },
+      },
+      {
+        fieldCode: {
+          type: "SINGLE_LINE_TEXT",
+          value: "value1",
+        },
+      },
+    ];
+    apiClient.record.getAllRecords = jest.fn().mockResolvedValue(records);
+    const actual = await exportRecords(apiClient, {
+      app: "1",
+      attachmentDir: "",
+    });
+    expect(actual).toStrictEqual(records);
+  });
   it.todo("can download files to a specified directory");
 });
