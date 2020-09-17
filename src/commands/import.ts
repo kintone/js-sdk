5e4b3dfb532dcdb38764361fa9f7333a93efc389
shisama <shisama07@gmail.com>
2020-09-17 17:24:15 +0900

    feat: import json file

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -1,12 +1,18 @@
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 import { AppID } from "@kintone/rest-api-client/lib/client/types";
-import { csvParser } from "../parser";
+import { parser } from "../parser";
 import { promises as fs } from "fs";
+import path from "path";
 
 type Options = {
   app: AppID;
   attachmentDir: string;
   filePath: string;
+};
+
+const extractFileType = (filepath: string) => {
+  // TODO this cannot detect file type without extensions
+  return path.extname(filepath).split(".").pop() || "";
 };
 
 export async function importRecords(
@@ -15,7 +21,8 @@ export async function importRecords(
 ) {
   const { app, filePath } = options;
   const buf = await fs.readFile(filePath);
-  const records = csvParser(buf.toString());
+  const type = extractFileType(filePath);
+  const records = parser(type, buf.toString());
   await apiClient.record.addAllRecords({
     app,
     records,
