232e688c18c79419d4f99ab961d8bc8a2a1d45dd
Toru Kobayashi <koba0004@gmail.com>
2020-09-18 16:13:38 +0900

    chore: pass compiling tsc

--- packages/dts-gen/src/utils/objectvalues.ts
+++ packages/dts-gen/src/utils/objectvalues.ts
@@ -1,7 +1,7 @@
 import { toPairs } from "lodash";
 
-export function objectValues(
-    object: Record<string, unknown>
+export function objectValues<T extends Record<string, any>>(
+    object: T
 ) {
     return toPairs(object).map((entry) => entry[1]);
 }
