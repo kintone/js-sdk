92c01b832cf622ae063ff06ed4a4d05202abf26f
Toru Kobayashi <koba0004@gmail.com>
2020-09-25 18:31:45 +0900

    chore: add babel.config.js for dts-gen

--- packages/dts-gen/jest.config.js
+++ packages/dts-gen/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  rootDir: "src",
-  testEnvironment: "node",
+    rootDir: "src",
+    testEnvironment: "node",
 };
--- packages/dts-gen/prettier.config.js
+++ packages/dts-gen/prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     tabWidth: 4,
     printWidth: 60,
-    trailingComma: "es5"
+    trailingComma: "es5",
 };
