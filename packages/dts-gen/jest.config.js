bfcee052aa5fc1e9713086ea59083d8d8ddb7ba3
Toru Kobayashi <koba0004@gmail.com>
2020-09-25 18:19:41 +0900

    chore: update README and jest settings

--- packages/dts-gen/jest.config.js
+++ packages/dts-gen/jest.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/src"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-      "node"
-    ],
-}
+  rootDir: "src",
+  testEnvironment: "node",
+};
