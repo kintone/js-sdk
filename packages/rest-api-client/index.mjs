fee66553b10ea779efd8e0a2809c3711957a6c99
Masashi Hirano <shisama07@gmail.com>
2020-10-12 19:17:35 +0900

    feat: support Node.js native ESM for rest-api-client (#436)
    * feat: support Node.js native ESM
    
    * chore: add "browser" field into packageJson.exports for webpack

---
+++ packages/rest-api-client/index.mjs
@@ -0,0 +1,5 @@
+import module from "module";
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+const require = module.createRequire(import.meta.url);
+
+export const { KintoneRestAPIClient } = require(".");
