a19949a0bd6aef27276c25cf843fa1181ae1b393
shisama <shisama07@gmail.com>
2020-10-16 10:48:24 +0900

    chore: add Node.js polyfills for webpack 5

--- packages/plugin-packer/webpack.config.js
+++ packages/plugin-packer/webpack.config.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./site/index.js",
@@ -8,13 +9,31 @@ module.exports = {
     path: path.resolve(__dirname, "docs", "dist"),
     filename: "bundle.js",
   },
-  node: {
-    fs: "empty",
-  },
   devServer: {
     contentBase: path.resolve(__dirname, "docs"),
     publicPath: "/dist/",
     watchContentBase: true,
     open: true,
   },
+  resolve: {
+    fallback: {
+      assert: false,
+      fs: false,
+      util: require.resolve("util"),
+      path: require.resolve("path-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      constants: require.resolve("constants-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_DEBUG": process.env.NODE_DEBUG,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process",
+    }),
+  ],
 };
