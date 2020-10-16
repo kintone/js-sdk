46c30d32a99d68cadf9b2424fe8f85b08a77adc0
shintaroNagata <48338353+shintaroNagata@users.noreply.github.com>
2020-10-17 04:12:37 +0900

    refactor: move the type definition for Layout (#473)

--- packages/rest-api-client/src/client/types/app/form.ts
+++ packages/rest-api-client/src/client/types/app/form.ts
@@ -1,7 +1,17 @@
 import { OneOf as FieldProperty } from "../../../KintoneFields/types/property";
+import {
+  Row,
+  Subtable,
+  Group,
+  Field,
+} from "../../../KintoneFields/types/layout";
 
 export type Properties = {
   [fieldCode: string]: FieldProperty;
 };
 
-export { Layout } from "../../../KintoneFields/types/layout";
+export type Layout = Array<
+  | Row<Field.OneOf[]>
+  | Subtable<Field.InSubtable[]>
+  | Group<Array<Row<Field.OneOf[]>>>
+>;
