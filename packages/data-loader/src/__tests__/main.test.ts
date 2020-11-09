e12d9215e9ddc82c2115e4a6beb5a551bbdefe82
Masashi Hirano <shisama07@gmail.com>
2020-11-09 10:36:55 +0900

    feat: throw error when unknown arguments are passed (#3)
    * fix: throw Error when wrong arguments for command are passed
    
    * feat: process.exit(1) when unknown arg passes
    
    * test: test commmand arguments with built files
    
    * test: fix test name
    
    * test: undefined argument pattern
    
    * test: check error messages
    
    * test: add cross-env for process.env.LC_ALL

---
+++ packages/data-loader/src/__tests__/main.test.ts
@@ -0,0 +1,46 @@
+import childProcess, { execSync } from "child_process";
+import { promisify } from "util";
+import path from "path";
+import fs from "fs";
+const projectRoot = path.resolve(__dirname, "../..");
+const exec = promisify(childProcess.exec);
+const packageJson = require(path.resolve(projectRoot, "package.json"));
+const mainFilePath = path.resolve(projectRoot, packageJson.bin);
+
+const checkRejectArg = ({
+  arg,
+  errorMessage,
+}: {
+  arg: string;
+  errorMessage: string;
+}) => {
+  return expect(
+    exec(`cross-env LC_ALL='en_US' node ${mainFilePath} ${arg}`)
+  ).rejects.toThrow(errorMessage);
+};
+
+describe("main", () => {
+  beforeEach(() => {
+    if (!fs.existsSync(mainFilePath)) {
+      execSync("npm run build");
+    }
+  });
+  it("should throw error when no commands are passed", () => {
+    return checkRejectArg({
+      arg: "",
+      errorMessage: "Not enough non-option arguments: got 0, need at least 1",
+    });
+  });
+  it("should throw error when an undefined command is passed", () => {
+    return checkRejectArg({
+      arg: "dummy",
+      errorMessage: "Unknown argument: dummy",
+    });
+  });
+  it("should throw error when an undefined argument is passed", () => {
+    return checkRejectArg({
+      arg: "import --dummy",
+      errorMessage: "Unknown argument: dummy",
+    });
+  });
+});
--- packages/data-loader/src/main.ts
+++ packages/data-loader/src/main.ts
@@ -1,5 +1,4 @@
 import yargs from "yargs";
 
-// FIXME: It doesn't display an error when not passing any arguments.
 // eslint-disable-next-line no-unused-expressions
-yargs.commandDir("commands").demandCommand().help().argv;
+yargs.commandDir("commands").demandCommand().strict().help().argv;
