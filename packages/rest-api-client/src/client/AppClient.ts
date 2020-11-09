a6f727f0e0b266a78c69eb7f3ae0d2df29379cbb
Hodaka Kasama <hodaka-kasama@cybozu.co.jp>
2020-11-09 10:03:14 +0900

    fix: Modify `properties` type in AppClient (#469)
    * modify property for parameter type in AppClient
    
    * tests: pass test method for new properties for parameter
    
    * fix: refactor name and logic in properties for parameter type

--- packages/rest-api-client/src/client/AppClient.ts
+++ packages/rest-api-client/src/client/AppClient.ts
@@ -44,6 +44,13 @@ type GroupLayoutForParameter = {
 type LayoutForParameter = Array<
   RowLayoutForParameter | SubtableLayoutForParameter | GroupLayoutForParameter
 >;
+type NestedPartial<T> = T extends object
+  ? {
+      [K in keyof T]?: NestedPartial<T[K]>;
+    }
+  : T;
+
+type PropertiesForParameter = NestedPartial<Properties>;
 
 export class AppClient {
   private client: HttpClient;
@@ -69,7 +76,7 @@ export class AppClient {
 
   public addFormFields(params: {
     app: AppID;
-    properties: object;
+    properties: PropertiesForParameter;
     revision?: Revision;
   }): Promise<{ revision: string }> {
     const path = this.buildPathWithGuestSpaceId({
@@ -81,7 +88,7 @@ export class AppClient {
 
   public updateFormFields(params: {
     app: AppID;
-    properties: object;
+    properties: PropertiesForParameter;
     revision?: Revision;
   }): Promise<{ revision: string }> {
     const path = this.buildPathWithGuestSpaceId({
