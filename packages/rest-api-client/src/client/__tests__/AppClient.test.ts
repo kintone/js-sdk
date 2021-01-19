bd0e3dc83cb52caf07c2e57f89ab89e725254109
shisama <shisama07@gmail.com>
2021-01-19 22:07:32 +0900

    feat: add PUT /k/v1/preview/app/notifications/reminder.json

--- packages/rest-api-client/src/client/__tests__/AppClient.test.ts
+++ packages/rest-api-client/src/client/__tests__/AppClient.test.ts
@@ -957,6 +957,66 @@ describe("AppClient", () => {
     });
   });
 
+  describe("updateReminderlNotifications", () => {
+    const params = {
+      app: 1,
+      notifications: [
+        {
+          timing: {
+            code: "CREATED_TIME",
+            daysLater: "1",
+            hoursLater: "2",
+          },
+          filterCond: 'CREATED_TIME in ("user1)',
+          title: "test title1",
+          targets: [
+            {
+              entity: {
+                type: "USER",
+                code: "user1",
+              } as const,
+              includeSubs: false,
+            },
+          ],
+        },
+        {
+          timing: {
+            code: "CREATED_TIME",
+            daysLater: "-3",
+            time: "08:30",
+          },
+          filterCond: 'CREATED_TIME in ("user1")',
+          title: "test title2",
+          targets: [
+            {
+              entity: {
+                type: "USER",
+                code: "user1",
+              } as const,
+              includeSubs: false,
+            },
+          ],
+        },
+      ],
+      timezone: "Asia/Tokyo",
+      revision: "2",
+    };
+    beforeEach(async () => {
+      await appClient.updateReminderlNotifications(params);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe(
+        "/k/v1/preview/app/notifications/reminder.json"
+      );
+    });
+    it("should send a put request", () => {
+      expect(mockClient.getLogs()[0].method).toBe("put");
+    });
+    it("should pass app and rights as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual(params);
+    });
+  });
+
   describe("updatePerRecordNotifications", () => {
     const params = {
       app: APP_ID,
