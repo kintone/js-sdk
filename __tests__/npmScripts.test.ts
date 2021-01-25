77efd924e29fac49e0af937241ed3f566a029862
Toru Kobayashi <koba0004@gmail.com>
2021-01-25 14:18:51 +0900

    chore: apply Prettier

--- __tests__/npmScripts.test.ts
+++ __tests__/npmScripts.test.ts
@@ -1,31 +1,31 @@
-import path from 'path';
-import { spawnSync } from 'child_process';
+import path from "path";
+import { spawnSync } from "child_process";
 
-const REQUIRED_NPMSCRIPTS = [
-  "build",
-  "lint",
-  "test",
-  "test:ci"
-]
+const REQUIRED_NPMSCRIPTS = ["build", "lint", "test", "test:ci"];
 
-const yarnCommand = process.platform === 'win32' ? 'yarn.cmd' : 'yarn';
+const yarnCommand = process.platform === "win32" ? "yarn.cmd" : "yarn";
 
 describe("npmScripts", () => {
   it("should define all required npm-scripts in all pacakges", () => {
-    const packageInfo = JSON.parse(spawnSync(yarnCommand, ["workspaces", "info"]).stdout.toString());
-    Object.entries<{ location: string }>(packageInfo).forEach(([name, {location}]) => {
-      if (location.indexOf("examples/") === 0) {
-        return;
-      }
-      const npmScripts = require(path.resolve(location, "package.json")).scripts;
-      REQUIRED_NPMSCRIPTS.forEach(script => {
-        try {
-          expect(typeof npmScripts[script]).toBe("string");
-        } catch (e) {
-          console.error(`${name} doesn't have "${script}" as a npm-scripts.`);
-          throw e;
+    const packageInfo = JSON.parse(
+      spawnSync(yarnCommand, ["workspaces", "info"]).stdout.toString()
+    );
+    Object.entries<{ location: string }>(packageInfo).forEach(
+      ([name, { location }]) => {
+        if (location.indexOf("examples/") === 0) {
+          return;
         }
-      });
-    })
-  })
-})
+        const npmScripts = require(path.resolve(location, "package.json"))
+          .scripts;
+        REQUIRED_NPMSCRIPTS.forEach((script) => {
+          try {
+            expect(typeof npmScripts[script]).toBe("string");
+          } catch (e) {
+            console.error(`${name} doesn't have "${script}" as a npm-scripts.`);
+            throw e;
+          }
+        });
+      }
+    );
+  });
+});
