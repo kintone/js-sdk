0f42303f8f163b07a348bea3895c2b529754f021
nakajmg <nakajima.jmg@gmail.com>
2021-02-15 11:17:19 +0900

    feat(data-loader): support information field

--- packages/data-loader/src/csvConverter/fromJSON.ts
+++ packages/data-loader/src/csvConverter/fromJSON.ts
@@ -1,4 +1,5 @@
 import { KintoneRecordField } from "@kintone/rest-api-client";
+import { format } from "util";
 
 type KintoneRecords = Array<{ [k: string]: KintoneRecordField.OneOf }>;
 
@@ -16,8 +17,30 @@ const isSupportedFieldType = (field: KintoneRecordField.OneOf) => {
     "LINK",
     "DROP_DOWN",
     "CALC",
+    "CREATOR",
+    "MODIFIER",
+    "UPDATED_TIME",
+    "CREATED_TIME",
   ];
   return supportedFieldTypes.includes(field.type);
+};
+
+const zeroPad = (num: number) => (num + "").padStart(2, "0");
+
+/**
+ * format date to "YYYY/MM/DD HH:mm"
+ * @param dateString
+ */
+const formatDateFieldValue = (dateString: string) => {
+  const date = new Date(dateString);
+  return format(
+    "%d/%d/%d %d:%d",
+    date.getFullYear(),
+    zeroPad(date.getMonth() + 1),
+    zeroPad(date.getDate()),
+    zeroPad(date.getHours()),
+    zeroPad(date.getMinutes())
+  );
 };
 
 const lexer = (field: KintoneRecordField.OneOf) => {
@@ -27,22 +50,30 @@ const lexer = (field: KintoneRecordField.OneOf) => {
     case "RADIO_BUTTON":
     case "MULTI_LINE_TEXT":
     case "NUMBER":
+    case "RICH_TEXT":
     case "LINK":
     case "DROP_DOWN":
     case "CALC":
       return encloseInQuotation(field.value);
+    case "CREATOR":
+    case "MODIFIER":
+      return encloseInQuotation(field.value.code);
+    case "UPDATED_TIME":
+    case "CREATED_TIME":
+      return encloseInQuotation(formatDateFieldValue(field.value));
     default:
       return field.value;
   }
 };
 
-const escapeQuotation = (value: string) => value.replace(/"/g, '""');
+const escapeQuotation = (fieldValue: string) => fieldValue.replace(/"/g, '""');
 
-const encloseInQuotation = (value: string | null) =>
-  value ? `"${escapeQuotation(value)}"` : "";
+const encloseInQuotation = (fieldValue: string | null) =>
+  `"${fieldValue ? escapeQuotation(fieldValue) : ""}"`;
 
 const extractFieldCodes = (records: KintoneRecords) => {
-  const firstRecord = records[0];
+  const firstRecord = records.slice().shift();
+  if (!firstRecord) return [];
   return Object.keys(firstRecord).filter((key) =>
     isSupportedFieldType(firstRecord[key])
   );
@@ -55,11 +86,14 @@ export const fromJSON = (records: KintoneRecords) => {
     .map((fieldCode) => encloseInQuotation(fieldCode))
     .join(SEPARATOR);
 
-  const rows = records.map((record) => {
-    return fieldCodes
-      .map((fieldCode) => lexer(record[fieldCode]))
-      .join(SEPARATOR);
-  });
+  const rows = records
+    .slice()
+    .reverse()
+    .map((record) => {
+      return fieldCodes
+        .map((fieldCode) => lexer(record[fieldCode]))
+        .join(SEPARATOR);
+    });
 
   return [header, ...rows].join(LINE_BREAK);
 };
