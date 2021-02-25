d730b5d153947d4fc9f06d877194498808b4c6e0
nakajmg <nakajima.jmg@gmail.com>
2021-02-25 17:28:03 +0900

    feat(data-loader): add format option

--- packages/data-loader/src/commands/import.ts
+++ packages/data-loader/src/commands/import.ts
@@ -45,6 +45,10 @@ export const builder = (yargs: any) =>
     .option("file-path", {
       describe: "file path",
     })
+    .option("format", {
+      describe: "Output format",
+      default: "json",
+    })
     .option("pfx-file-path", {
       describe: "The path to client certificate file",
     })
