a278f68d8cb3940e323b43e1724a9668b6defd52
chick-p <chick.p.1120@gmail.com>
2021-03-09 08:08:59 +0900

    refactor: type of report

--- packages/rest-api-client/src/client/types/app/report.ts
+++ packages/rest-api-client/src/client/types/app/report.ts
@@ -3,54 +3,73 @@ export type ReportForResponse = {
   [graphName: string]: GraphProperty;
 };
 
-type Period = "YEAR" | "QUARTER" | "MONTH" | "WEEK" | "DAY" | "HOUR" | "MINUTE";
+type ChartType =
+  | "BAR"
+  | "COLUMN"
+  | "PIE"
+  | "LINE"
+  | "PIVOT_TABLE"
+  | "TABLE"
+  | "AREA"
+  | "SPLINE"
+  | "SPLINE_AREA";
+
+type ChartMode = "NORMAL" | "STACKED" | "PERCENTAGE";
+
+type PeriodicReportPeriodEvery =
+  | "YEAR"
+  | "QUARTER"
+  | "MONTH"
+  | "WEEK"
+  | "DAY"
+  | "HOUR"
+  | "MINUTE";
+
 type Aggregation = {
   type: "COUNT" | "SUM" | "AVERAGE" | "MAX" | "MIN";
   code: string;
 };
+
 type Sort = {
   by: "TOTAL" | "GROUP1" | "GROUP2" | "GROUP3";
   order: "ASC" | "DESC";
 };
-type PeriodicReport = {
+
+type QuarterPattern = "JAN_APR_JUL_OCT" | "FEB_MAY_AUG_NOV" | "MAR_JUN_SEP_DEC";
+
+type DayOfWeek =
+  | "SUNDAY"
+  | "MONDAY"
+  | "TUESDAY"
+  | "WEDNESDAY"
+  | "THURSDAY"
+  | "FRIDAY"
+  | "SATURDAY";
+
+type PeriodicReportForParameters = {
   active?: boolean | string;
   period?: {
-    every: Period;
+    every: PeriodicReportPeriodEvery;
     month?: number | string;
     time?: string;
-    pattern?: "JAN_APR_JUL_OCT" | "FEB_MAY_AUG_NOV" | "MAR_JUN_SEP_DEC";
+    pattern?: QuarterPattern;
     dayOfMonth?: string | "END_OF_MONTH";
-    dayOfWeek?:
-      | "SUNDAY"
-      | "MONDAY"
-      | "TUESDAY"
-      | "WEDNESDAY"
-      | "THURSDAY"
-      | "FRIDAY"
-      | "SATURDAY";
+    dayOfWeek?: DayOfWeek;
     minute?: string;
   };
 };
+
 type GraphProperty = {
-  chartType:
-    | "BAR"
-    | "COLUMN"
-    | "PIE"
-    | "LINE"
-    | "PIVOT_TABLE"
-    | "TABLE"
-    | "AREA"
-    | "SPLINE"
-    | "SPLINE_AREA";
-  chartMode?: "NORMAL" | "STACKED" | "PERCENTAGE";
+  chartType: ChartType;
+  chartMode?: ChartMode;
   name: string;
   index: string | number;
   groups?: Array<{
     code: string;
-    per: Period;
+    per: PeriodicReportPeriodEvery;
   }>;
   aggregations?: Aggregation[];
   filterCond?: string;
   sorts?: Sort[];
-  periodicReport?: PeriodicReport;
+  periodicReport?: PeriodicReportForParameters;
 };
