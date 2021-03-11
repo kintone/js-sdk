7163785e7c6e2dbb073ea1bb7db3362e15e6af0b
nakajmg <nakajima.jmg@gmail.com>
2021-03-11 11:47:18 +0900

    fix(data-loader): meybe fixed

--- packages/data-loader/src/printers/csvPrinter.ts
+++ packages/data-loader/src/printers/csvPrinter.ts
@@ -81,7 +81,7 @@ export const convertKintoneRecordsToCsv = (records: KintoneRecords) => {
     });
 
   return (
-    [header, ...rows].join(LINE_BREAK).replace(/\r?\n/gm, LINE_BREAK) +
+    [header, ...rows].join(LINE_BREAK).replace(/\r?\n/g, LINE_BREAK) +
     LINE_BREAK
   );
 };
