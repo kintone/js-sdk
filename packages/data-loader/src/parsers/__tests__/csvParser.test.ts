545d14a9c0a70b2e905c20c8646c5ba6d9eaf35f
shisama <shisama07@gmail.com>
2021-03-12 15:39:37 +0900

    test: readFile -> ReadableStream

--- packages/data-loader/src/parsers/__tests__/csvParser.test.ts
+++ packages/data-loader/src/parsers/__tests__/csvParser.test.ts
@@ -5,6 +5,7 @@ import {
 import { parseCsv } from "../csvParser";
 import fs from "fs";
 import path from "path";
+import readline from "readline";
 
 const outputJson: Array<{
   [k: string]: KintoneRecordField.OneOf;
@@ -15,10 +16,24 @@ const fieldsJson: {
 
 describe("csvParser", () => {
   it("should convert csv string to JSON correctly", async () => {
-    const csv = await fs.promises.readFile(
-      path.resolve(__dirname, "fixtures", "records.csv"),
-      "utf-8"
-    );
-    expect(parseCsv(csv.replace(/((?!.)\s)+/g, "\n"), fieldsJson)).toEqual(outputJson);
+    // const csv = await fs.promises.readFile(
+    //   path.resolve(__dirname, "fixtures", "records.csv"),
+    //   "utf-8"
+    // );
+    // expect(parseCsv(csv.replace(/((?!.)\s)+/g, "\n"), fieldsJson)).toEqual(outputJson);
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream(path.resolve(__dirname, "fixtures", "records.csv")),
+      output: process.stdout,
+      terminal: false
+    });
+    let csv = "";
+    rl.on("line", (line) => {
+      csv += line + "\n";
+    });
+    rl.once("close", () => {
+      const actual = parseCsv(csv, fieldsJson);
+      expect(actual).toEqual(outputJson)
+    })
   });
 });
