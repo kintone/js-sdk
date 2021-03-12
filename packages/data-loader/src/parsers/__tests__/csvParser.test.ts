8115def117805ffc2b87b69f9d4bd71f09d7eb4c
nakajmg <nakajima.jmg@gmail.com>
2021-03-12 14:18:06 +0900

    test(data-loader): use csv string instead of file

--- packages/data-loader/src/parsers/__tests__/csvParser.test.ts
+++ packages/data-loader/src/parsers/__tests__/csvParser.test.ts
@@ -2,8 +2,6 @@ import {
   KintoneRecordField,
   KintoneFormFieldProperty,
 } from "@kintone/rest-api-client";
-import fs from "fs";
-import path from "path";
 import { parseCsv } from "../csvParser";
 const outputJson: Array<{
   [k: string]: KintoneRecordField.OneOf;
@@ -12,12 +10,15 @@ const fieldsJson: {
   properties: { [k: string]: KintoneFormFieldProperty.OneOf };
 } = require("./fixtures/fields.json");
 
+const inputCsv = `"recordNumber","updatedTime","dropDown","creator","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
+"9","2021-02-16T02:43:00Z","sample1","username","username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4,sample5"
+`;
+
 describe("csvParser", () => {
   it("should convert csv string to JSON correctly", async () => {
-    const csv = await fs.promises.readFile(
-      path.resolve(__dirname, "fixtures", "records.csv"),
-      "utf-8"
-    );
-    expect(parseCsv(csv, fieldsJson)).toEqual(outputJson);
+    expect(parseCsv(inputCsv, fieldsJson)).toEqual(outputJson);
   });
 });
