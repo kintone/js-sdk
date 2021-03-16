9626aaa91a863bc3ce957e04f95f5597c9e1a1e3
piyo <chick.p.1120@gmail.com>
2021-03-17 08:49:54 +0900

    fix(rest-api-client): attach strict type for updateReports

--- packages/rest-api-client/src/client/types/app/report.ts
+++ packages/rest-api-client/src/client/types/app/report.ts
@@ -1,22 +1,42 @@
-export type ReportForParameter = {};
-export type ReportForResponse = {
-  [graphName: string]: GraphProperty;
+export type ReportForParameter = {
+  [reportName: string]: ReportProperty;
 };
 
-type ChartType =
-  | "BAR"
-  | "COLUMN"
+type ChartTypeWithRequiredMode = "BAR" | "COLUMN" | "AREA" | "SPLINE_AREA";
+
+type ChartTypeWithOptionalMode =
   | "PIE"
   | "LINE"
   | "PIVOT_TABLE"
   | "TABLE"
-  | "AREA"
-  | "SPLINE"
-  | "SPLINE_AREA";
+  | "SPLINE";
+
+type ChartProps = {
+  name: string;
+  index: string | number;
+  groups?: Array<{
+    code: string;
+    per?: GroupPer;
+  }>;
+  aggregations?: Aggregation[];
+  filterCond?: string;
+  sorts?: Sort[];
+  periodicReport?: PeriodicReportForParameters;
+};
+
+type ReportProperty =
+  | ({
+      chartType: ChartTypeWithOptionalMode;
+      chartMode?: ChartMode;
+    } & ChartProps)
+  | ({
+      chartType: ChartTypeWithRequiredMode;
+      chartMode: ChartMode;
+    } & ChartProps);
 
 type ChartMode = "NORMAL" | "STACKED" | "PERCENTAGE";
 
-type PeriodicReportPeriodEvery =
+type GroupPer =
   | "YEAR"
   | "QUARTER"
   | "MONTH"
@@ -25,15 +45,68 @@ type PeriodicReportPeriodEvery =
   | "HOUR"
   | "MINUTE";
 
-type Aggregation = {
-  type: "COUNT" | "SUM" | "AVERAGE" | "MAX" | "MIN";
-  code: string;
-};
+type Aggregation =
+  | {
+      type: "COUNT";
+    }
+  | {
+      type: "SUM" | "AVERAGE" | "MAX" | "MIN";
+      code: string;
+    };
 
 type Sort = {
   by: "TOTAL" | "GROUP1" | "GROUP2" | "GROUP3";
   order: "ASC" | "DESC";
 };
+
+type PeriodicReportForParameters = {
+  active?: boolean | string;
+  period?:
+    | PeriodReportPeriodYear
+    | PeriodReportPeriodDay
+    | PeriodReportPeriodQuater
+    | PeriodReportPeriodMonth
+    | PeriodReportPeriodWeek
+    | PeriodReportPeriodHour;
+};
+
+type PeriodReportPeriodYear = {
+  every: "YEAR";
+  month: MonthOfYear;
+  time: string;
+  dayOfMonth: DayOfMonth;
+};
+
+type PeriodReportPeriodQuater = {
+  every: "QUARTER";
+  pattern: QuarterPattern;
+  time: string;
+  dayOfMonth: DayOfMonth | EndOfMonth;
+};
+
+type PeriodReportPeriodMonth = {
+  every: "MONTH";
+  time: string;
+  dayOfMonth: DayOfMonth | EndOfMonth;
+};
+
+type PeriodReportPeriodWeek = {
+  every: "WEEK";
+  dayOfWeek: DayOfWeek;
+  time: string;
+};
+
+type PeriodReportPeriodDay = {
+  every: "DAY";
+  time: string;
+};
+
+type PeriodReportPeriodHour = {
+  every: "HOUR";
+  minute: MinuteOfHour;
+};
+
+type EndOfMonth = "END_OF_MONTH";
 
 type QuarterPattern = "JAN_APR_JUL_OCT" | "FEB_MAY_AUG_NOV" | "MAR_JUN_SEP_DEC";
 
@@ -46,30 +119,51 @@ type DayOfWeek =
   | "FRIDAY"
   | "SATURDAY";
 
-type PeriodicReportForParameters = {
-  active?: boolean | string;
-  period?: {
-    every: PeriodicReportPeriodEvery;
-    month?: number | string;
-    time?: string;
-    pattern?: QuarterPattern;
-    dayOfMonth?: string | "END_OF_MONTH";
-    dayOfWeek?: DayOfWeek;
-    minute?: string;
-  };
-};
+type DayOfMonth =
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "11"
+  | "12"
+  | "13"
+  | "14"
+  | "15"
+  | "16"
+  | "17"
+  | "18"
+  | "19"
+  | "20"
+  | "21"
+  | "22"
+  | "23"
+  | "24"
+  | "25"
+  | "26"
+  | "27"
+  | "28"
+  | "29"
+  | "30"
+  | "31";
 
-type GraphProperty = {
-  chartType: ChartType;
-  chartMode?: ChartMode;
-  name: string;
-  index: string | number;
-  groups?: Array<{
-    code: string;
-    per: PeriodicReportPeriodEvery;
-  }>;
-  aggregations?: Aggregation[];
-  filterCond?: string;
-  sorts?: Sort[];
-  periodicReport?: PeriodicReportForParameters;
-};
+type MinuteOfHour = "0" | "10" | "20" | "30" | "40" | "50";
+
+type MonthOfYear =
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "11"
+  | "12";
