91fd9f10939ebad3a718fed5343481c1279ea0d1
nakajmg <nakajima.jmg@gmail.com>
2021-03-24 15:51:09 +0900

    test: use csv string instead of file

--- packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
+++ packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
@@ -9,20 +9,28 @@ const subTableRecords: KintoneRecord[] = require("./fixtures/subtable_input.json
 const subTableFieldsJson: FieldsJson = require("./fixtures/subtable_fields.json");
 
 describe("convertKintoneRecordsToCsv", () => {
-  it("should convert kintone records to csv string correctly", async () => {
-    const expectedCsv = await fs.promises.readFile(
-      path.resolve(__dirname, "fixtures", "records.csv"),
-      "utf8"
-    );
+  it("should convert kintone records to csv string correctly", () => {
+    const expectedCsv = `"recordNumber","updatedTime","dropDown","creator","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
+"9","2021-02-16T02:43:00Z","sample1","username","username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4"
+`;
     expect(convertKintoneRecordsToCsv({ records, fieldsJson })).toBe(
       expectedCsv
     );
   });
-  it("should convert kintone records to csv string correctly when SUBTABLE included", async () => {
-    const expectedCsv = await fs.promises.readFile(
-      path.resolve(__dirname, "fixtures", "subtable_records.csv"),
-      "utf8"
-    );
+  it("should convert kintone records to csv string correctly when SUBTABLE included", () => {
+    const expectedCsv = `"*","__index__","recordNumber","updatedTime","dropDown","creator","subTable.id","subTable.subTableText","subTable.subTableCheckbox","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
+*,"1","9","2021-02-16T02:43:00Z","sample1","username","537306","text_line1","st_sample1","username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4"
+,"1","9","2021-02-16T02:43:00Z","sample1","username","537307","text_line2","st_sample2","username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4"
+`;
     expect(
       convertKintoneRecordsToCsv({
         records: subTableRecords,
