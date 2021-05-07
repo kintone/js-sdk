8487c12ecb88922b5612270d9489571cfab5c354
tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>
2021-05-07 02:08:37 +0000

    fix(data-loader): apply accurated types to CLI command builder (#832)
    * fix(data-loader): apply types to command builder
    
    * fix(data-loader): infer the parameter type of handler from return type of builder
    
    * fix(data-loader): use never instead of unknown for unreachable type
    
    * fix(data-loader): extract demandOption
    
    * refactor(data-loader): fix note comment
    
    Update packages/data-loader/src/commands/import.ts
    
    Co-authored-by: Shingo Yamazaki <shingoyamazaki00@gmail.com>
    
    Co-authored-by: Shingo Yamazaki <shingoyamazaki00@gmail.com>

--- packages/data-loader/src/commands/export.ts
+++ packages/data-loader/src/commands/export.ts
@@ -1,7 +1,5 @@
-import { run, Options, ExportFileFormat } from "../controllers/export";
-import { RestAPIClientOptions } from "../api";
-
-type Argv = RestAPIClientOptions & Options;
+import { run, ExportFileFormat } from "../controllers/export";
+import * as yargs from "yargs";
 
 const formats: ExportFileFormat[] = ["json", "csv"];
 
@@ -9,14 +7,13 @@ export const command = "export";
 
 export const desc = "export the records of the specified app";
 
-export const builder = (yargs: any) =>
-  yargs
+export const builder = (args: yargs.Argv) =>
+  args
     .option("base-url", {
       describe: "Kintone Base Url",
       default: process.env.KINTONE_BASE_URL,
       defaultDescription: "KINTONE_BASE_URL",
       type: "string",
-      demandOption: true,
     })
     .option("username", {
       alias: "u",
@@ -53,7 +50,6 @@ export const builder = (yargs: any) =>
     .option("app", {
       describe: "The ID of the app",
       type: "string",
-      demandOption: true,
     })
     .option("guest-space-id", {
       describe: "The ID of guest space",
@@ -82,6 +78,31 @@ export const builder = (yargs: any) =>
     .option("pfx-file-password", {
       describe: "The password of client certificate file",
       type: "string",
-    });
+    })
+    // NOTE: Since yargs doesn't detect the type correctly by adding `demandOption: true` in `option()`,
+    // (inferred type always contains `| undefined`)
+    // related issue: https://github.com/yargs/yargs/issues/1928
+    // we declare the required params later as a workaround.
+    .demandOption(["base-url", "app"]);
 
-export const handler = (argv: Argv) => run(argv);
+type Args = yargs.Arguments<
+  ReturnType<typeof builder> extends yargs.Argv<infer U> ? U : never
+>;
+
+export const handler = (args: Args) => {
+  return run({
+    baseUrl: args["base-url"],
+    username: args.username,
+    password: args.password,
+    apiToken: args["api-token"],
+    basicAuthUsername: args["basic-auth-username"],
+    basicAuthPassword: args["basic-auth-password"],
+    app: args.app,
+    guestSpaceId: args["guest-space-id"],
+    attachmentDir: args["attachment-dir"],
+    format: args.format,
+    query: args.query,
+    pfxFilePath: args["pfx-file-path"],
+    pfxFilePassword: args["pfx-file-password"],
+  });
+};
