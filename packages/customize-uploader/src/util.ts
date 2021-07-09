f704faf7bef8446d007680a52d3b0553f7928b24
piyo <chick.p.1120@gmail.com>
2021-07-09 13:34:30 +0900

    fix(customize-uploader): pass the value of `--proxy` (#1002)
    * fix(customize-uploader): should pass --proxy option
    
    * test(customize-uploader): add test for parseProxy

--- packages/customize-uploader/src/util.ts
+++ packages/customize-uploader/src/util.ts
@@ -5,3 +5,23 @@ export function isUrlString(str: string): boolean {
 export async function wait(ms: number): Promise<void> {
   await new Promise((r) => setTimeout(r, ms));
 }
+
+export function parseProxy(proxy: string): {
+  host: string;
+  port: number;
+  auth?: {
+    username: string;
+    password: string;
+  };
+} {
+  const { hostname, port, username, password } = new URL(proxy);
+  let auth;
+  if (username && password) {
+    auth = { username, password };
+  }
+  return {
+    host: hostname,
+    port: Number(port),
+    auth,
+  };
+}
