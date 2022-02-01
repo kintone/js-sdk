6144b11efe8723e7376f4ffa23b37ab3db1c1400
kjmnmt <kogen.toki+github@gmail.com>
2022-02-01 21:47:51 +0900

    test(data-loader): add test of without attachments-dir option case

--- packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
+++ packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
@@ -5,6 +5,7 @@ import { DataLoaderRecord } from "../../../types/data-loader";
 const records: DataLoaderRecord[] = require("./fixtures/input.json");
 const fieldsJson: FieldsJson = require("./fixtures/fields.json");
 const fileRecords: DataLoaderRecord[] = require("./fixtures/file_input.json");
+const fileWithoutAttachmentsDirRecords: DataLoaderRecord[] = require("./fixtures/file_without_attachments_dir.json");
 const fileFieldsJson: FieldsJson = require("./fixtures/file_fields.json");
 const subtableRecords: DataLoaderRecord[] = require("./fixtures/subtable_input.json");
 const subtableFieldsJson: FieldsJson = require("./fixtures/subtable_fields.json");
@@ -37,6 +38,20 @@ file-9/test (1).txt"
     expect(
       convertRecordsToCsv({
         records: fileRecords,
+        fieldProperties: fileFieldsJson.properties,
+      })
+    ).toBe(expectedCsv);
+  });
+  it("should convert kintone records to csv string correctly when FILE included and without attachmentsDir option", () => {
+    const expectedCsv = `"recordNumber","updatedTime","dropDown","creator","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect","file"
+"9","2021-02-16T02:43:00Z","sample1","username","username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4",""
+`;
+    expect(
+      convertRecordsToCsv({
+        records: fileWithoutAttachmentsDirRecords,
         fieldProperties: fileFieldsJson.properties,
       })
     ).toBe(expectedCsv);
