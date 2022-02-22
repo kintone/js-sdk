f84fbe3f64b3d091297a410dd733c7bd1e88a2f4
ksm-cy <yczhou@cybozu.cn>
2022-02-22 19:03:27 +0900

    test(data-loader): add test for Table with empty array value

--- packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
+++ packages/data-loader/src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
@@ -9,6 +9,7 @@ const fileWithoutAttachmentsDirRecords: DataLoaderRecord[] = require("./fixtures
 const fileFieldsJson: FieldsJson = require("./fixtures/file_fields.json");
 const subtableRecords: DataLoaderRecord[] = require("./fixtures/subtable_input.json");
 const subtableWithoutAttachmentsDirRecords: DataLoaderRecord[] = require("./fixtures/subtable_input_without_attachments_dir.json");
+const subtableWithEmptyTable: DataLoaderRecord[] = require("./fixtures/subtable_input_with_empty_table.json");
 const subtableFieldsJson: FieldsJson = require("./fixtures/subtable_fields.json");
 
 describe("convertKintoneRecordsToCsv", () => {
@@ -95,6 +96,20 @@ sample4"
     expect(
       convertRecordsToCsv({
         records: subtableWithoutAttachmentsDirRecords,
+        fieldProperties: subtableFieldsJson.properties,
+      })
+    ).toBe(expectedCsv);
+  });
+  it("should convert kintone records to csv string correctly when SUBTABLE value is an empty array", () => {
+    const expectedCsv = `"*","recordNumber","updatedTime","dropDown","creator","subTable","subTableText","subTableCheckbox","subTableFile","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
+*,"9","2021-02-16T02:43:00Z","sample1","username",,,,,"username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+line
+text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
+sample4"
+`;
+    expect(
+      convertRecordsToCsv({
+        records: subtableWithEmptyTable,
         fieldProperties: subtableFieldsJson.properties,
       })
     ).toBe(expectedCsv);
