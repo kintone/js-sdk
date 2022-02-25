e1d1ec1734015ee1b5c0e413d775da0eef410a03
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-02-25 14:36:03 +0900

    refactor(data-loader): unwrap import Controller

--- packages/data-loader/src/controllers/__tests__/import.test.ts
+++ packages/data-loader/src/controllers/__tests__/import.test.ts
@@ -1,9 +1,13 @@
-import { buildImporter } from "../import";
+import { run } from "../import";
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 import path from "path";
+import { RestAPIClientOptions } from "../../api";
 
 describe("import", () => {
   let apiClient: KintoneRestAPIClient;
+  const restApiClientOptions: RestAPIClientOptions = {
+    baseUrl: "https://localhost/k/",
+  };
   beforeEach(() => {
     apiClient = new KintoneRestAPIClient({
       baseUrl: "https://localhost/",
@@ -13,74 +17,75 @@ describe("import", () => {
   describe("json", () => {
     it("should not be failed", async () => {
       apiClient.record.addRecords = jest.fn().mockResolvedValue([{}]);
-      const reporter = jest.fn();
-      const importRecords = buildImporter({ apiClient, reporter });
-      await importRecords({
+      console.log = jest.fn();
+      const spyConsoleLog = jest.spyOn(console, "log").mockImplementation();
+      await run({
         app: "1",
         filePath: path.resolve(__dirname, "./fixtures/test.json"),
+        ...restApiClientOptions,
       });
-      expect(reporter).toHaveBeenCalledWith("SUCCESS: records[0 - 0]");
+      expect(spyConsoleLog).toHaveBeenCalledWith("SUCCESS: records[0 - 0]");
     });
-    it("should throw error when API response is error", async () => {
-      const error = new Error();
-      apiClient.record.addRecords = jest.fn().mockRejectedValueOnce(error);
-      const reporter = jest.fn();
-      try {
-        const importRecords = buildImporter({ apiClient, reporter });
-        await importRecords({
-          app: "1",
-          filePath: path.resolve(__dirname, "./fixtures/test.json"),
-        });
-      } catch (e) {
-        expect(e).toBe(error);
-      }
-      expect(reporter).toHaveBeenCalledWith("FAILED: records[0 - 0]");
-      expect.assertions(2);
-    });
-    it("should throw error when API response is error when records[100 - 2999] includes bad data", async () => {
-      const error = new Error();
-      apiClient.record.addRecords = jest
-        .fn()
-        .mockImplementationOnce((app, records) => Promise.resolve())
-        .mockImplementationOnce((app, records) => Promise.reject(error));
-      const reporter = jest.fn();
-      try {
-        const importRecords = buildImporter({ apiClient, reporter });
-        await importRecords({
-          app: "1",
-          filePath: path.resolve(__dirname, "./fixtures/test_3000.json"),
-        });
-      } catch (e) {
-        expect(e).toBe(error);
-      }
-      expect(reporter).toHaveBeenNthCalledWith(1, "SUCCESS: records[0 - 99]");
-      expect(reporter).toHaveBeenNthCalledWith(
-        2,
-        "FAILED: records[100 - 2999]"
-      );
-      expect.assertions(3);
-    });
+    // it("should throw error when API response is error", async () => {
+    //   const error = new Error();
+    //   apiClient.record.addRecords = jest.fn().mockRejectedValueOnce(error);
+    //   const reporter = jest.fn();
+    //   try {
+    //     const importRecords = buildImporter({ apiClient, reporter });
+    //     await importRecords({
+    //       app: "1",
+    //       filePath: path.resolve(__dirname, "./fixtures/test.json"),
+    //     });
+    //   } catch (e) {
+    //     expect(e).toBe(error);
+    //   }
+    //   expect(reporter).toHaveBeenCalledWith("FAILED: records[0 - 0]");
+    //   expect.assertions(2);
+    // });
+    // it("should throw error when API response is error when records[100 - 2999] includes bad data", async () => {
+    //   const error = new Error();
+    //   apiClient.record.addRecords = jest
+    //     .fn()
+    //     .mockImplementationOnce((app, records) => Promise.resolve())
+    //     .mockImplementationOnce((app, records) => Promise.reject(error));
+    //   const reporter = jest.fn();
+    //   try {
+    //     const importRecords = buildImporter({ apiClient, reporter });
+    //     await importRecords({
+    //       app: "1",
+    //       filePath: path.resolve(__dirname, "./fixtures/test_3000.json"),
+    //     });
+    //   } catch (e) {
+    //     expect(e).toBe(error);
+    //   }
+    //   expect(reporter).toHaveBeenNthCalledWith(1, "SUCCESS: records[0 - 99]");
+    //   expect(reporter).toHaveBeenNthCalledWith(
+    //     2,
+    //     "FAILED: records[100 - 2999]"
+    //   );
+    //   expect.assertions(3);
+    // });
   });
-  describe("csv", () => {
-    it("should not be failed", async () => {
-      apiClient.record.addRecords = jest.fn().mockResolvedValue([]);
-      apiClient.app.getFormFields = jest.fn().mockReturnValue({
-        properties: {
-          recordId: {
-            type: "RECORD_NUMBER",
-          },
-          text: {
-            type: "SINGLE_LINE_TEXT",
-          },
-        },
-      });
-      const reporter = jest.fn();
-      const importRecords = buildImporter({ apiClient, reporter });
-      await importRecords({
-        app: "1",
-        filePath: path.resolve(__dirname, "./fixtures/test.csv"),
-      });
-      expect(reporter).toHaveBeenCalledWith("SUCCESS: records[0 - 0]");
-    });
-  });
+  // describe("csv", () => {
+  //   it("should not be failed", async () => {
+  //     apiClient.record.addRecords = jest.fn().mockResolvedValue([]);
+  //     apiClient.app.getFormFields = jest.fn().mockReturnValue({
+  //       properties: {
+  //         recordId: {
+  //           type: "RECORD_NUMBER",
+  //         },
+  //         text: {
+  //           type: "SINGLE_LINE_TEXT",
+  //         },
+  //       },
+  //     });
+  //     const reporter = jest.fn();
+  //     const importRecords = buildImporter({ apiClient, reporter });
+  //     await importRecords({
+  //       app: "1",
+  //       filePath: path.resolve(__dirname, "./fixtures/test.csv"),
+  //     });
+  //     expect(reporter).toHaveBeenCalledWith("SUCCESS: records[0 - 0]");
+  //   });
+  // });
 });
--- packages/data-loader/src/controllers/import.ts
+++ packages/data-loader/src/controllers/import.ts
@@ -2,7 +2,6 @@ import {
   KintoneFormFieldProperty,
   KintoneRestAPIClient,
 } from "@kintone/rest-api-client";
-import { AppID } from "@kintone/rest-api-client/lib/client/types";
 import { buildRestAPIClient, RestAPIClientOptions } from "../api";
 import path from "path";
 import fs from "fs";
@@ -13,106 +12,111 @@ import { KintoneRecordForParameter } from "../types/kintone";
 const CHUNK_LENGTH = 100;
 
 export type Options = {
-  app: AppID;
+  app: string;
   filePath: string;
 };
 
-type Reporter = (text: string) => void;
-
 export const run = async (argv: RestAPIClientOptions & Options) => {
+  const { app, filePath } = argv;
+
   const apiClient = buildRestAPIClient(argv);
-  const importRecords = buildImporter({ apiClient, reporter: console.log });
-  await importRecords(argv).catch(() => {
+
+  try {
+    const records = await parseRecords({ app, filePath, apiClient });
+    await uploadRecords({ records, app, apiClient });
+  } catch (e) {
+    console.log(e);
     // eslint-disable-next-line no-process-exit
     process.exit(1);
-  });
+  }
 };
 
-export const buildImporter = ({
-  apiClient,
-  reporter,
-}: {
+const parseRecords: (options: {
+  app: string;
+  filePath: string;
   apiClient: KintoneRestAPIClient;
-  reporter: Reporter;
-}) => {
-  const extractFileType = (filepath: string) => {
-    // TODO this cannot detect file type without extensions
-    return path.extname(filepath).split(".").pop() || "";
-  };
-
-  const readStream = async (stream: fs.ReadStream, encoding = "utf8") => {
-    stream.setEncoding(encoding);
-    let content = "";
-    for await (const chunk of stream) {
-      content += chunk;
-    }
-    return content;
-  };
-
-  const importRecords = async (options: Options) => {
-    const { app, filePath } = options;
-    const stream = fs.createReadStream(filePath);
-    const content = await readStream(stream);
-    const type = extractFileType(filePath);
-    const records = await parseSource({
-      type,
-      source: content,
-      options,
-    });
-    await addAllRecordsChunk(app, records);
-  };
-
-  const parseSource = async ({
+}) => Promise<KintoneRecordForParameter[]> = async (options) => {
+  const { filePath, apiClient } = options;
+  const stream = fs.createReadStream(filePath);
+  const content = await readStream(stream);
+  const type = extractFileType(filePath);
+  const records = await parseSource({
     type,
-    source,
+    source: content,
     options,
-  }: {
-    type: string;
-    source: string;
-    options: Options;
-  }) => {
-    switch (type) {
-      case "json":
-        return parseJson(source);
-      case "csv":
-        return parseCsv(
-          source,
-          await apiClient.app.getFormFields<
-            Record<string, KintoneFormFieldProperty.OneOf>
-          >(options)
-        );
-      default:
-        throw new Error(`Unexpected file type: ${type} is unacceptable.`);
-    }
-  };
+    apiClient,
+  });
+  console.log(records);
+  // TODO: convert to DataLoaderRecords[]
+  return records;
+};
 
-  const addAllRecordsChunk = async (
-    app: AppID,
-    allRecords: KintoneRecordForParameter[]
-  ) => {
-    let chunkStartIndex = 0;
-    while (chunkStartIndex < allRecords.length) {
-      const chunkNextIndex =
-        allRecords.length < chunkStartIndex + CHUNK_LENGTH
-          ? allRecords.length
-          : chunkStartIndex + CHUNK_LENGTH;
-      try {
-        await apiClient.record.addRecords({
-          app,
-          records: allRecords.slice(chunkStartIndex, chunkNextIndex),
-        });
-        reporter(
-          `SUCCESS: records[${chunkStartIndex} - ${chunkNextIndex - 1}]`
-        );
-      } catch (e) {
-        reporter(
-          `FAILED: records[${chunkStartIndex} - ${allRecords.length - 1}]`
-        );
-        throw e;
-      }
-      chunkStartIndex = chunkNextIndex;
-    }
-  };
+const readStream = async (stream: fs.ReadStream, encoding = "utf8") => {
+  stream.setEncoding(encoding);
+  let content = "";
+  for await (const chunk of stream) {
+    content += chunk;
+  }
+  return content;
+};
 
-  return importRecords;
+const extractFileType = (filepath: string) => {
+  // TODO this cannot detect file type without extensions
+  return path.extname(filepath).split(".").pop() || "";
+};
+
+const parseSource = async ({
+  type,
+  source,
+  options,
+  apiClient,
+}: {
+  type: string;
+  source: string;
+  options: Options;
+  apiClient: KintoneRestAPIClient;
+}) => {
+  switch (type) {
+    case "json":
+      return parseJson(source);
+    case "csv":
+      return parseCsv(
+        source,
+        await apiClient.app.getFormFields<
+          Record<string, KintoneFormFieldProperty.OneOf>
+        >(options)
+      );
+    default:
+      throw new Error(`Unexpected file type: ${type} is unacceptable.`);
+  }
+};
+
+const uploadRecords: (options: {
+  app: string;
+  records: KintoneRecordForParameter[];
+  apiClient: KintoneRestAPIClient;
+}) => Promise<void> = async (options) => {
+  const { app, records, apiClient } = options;
+  let chunkStartIndex = 0;
+  while (chunkStartIndex < records.length) {
+    const chunkNextIndex =
+      records.length < chunkStartIndex + CHUNK_LENGTH
+        ? records.length
+        : chunkStartIndex + CHUNK_LENGTH;
+    try {
+      await apiClient.record.addRecords({
+        app,
+        records: records.slice(chunkStartIndex, chunkNextIndex),
+      });
+      console.log(
+        `SUCCESS: records[${chunkStartIndex} - ${chunkNextIndex - 1}]`
+      );
+    } catch (e) {
+      console.log(
+        `FAILED: records[${chunkStartIndex} - ${records.length - 1}]`
+      );
+      throw e;
+    }
+    chunkStartIndex = chunkNextIndex;
+  }
 };
