acb3d288d4103b45cde9a0e7f5c56b0e71c719c7
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-02-25 15:46:03 +0900

    test(data-loader): fix broken fixure

--- src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
+++ src/printers/printAsCsv/__tests__/convertKintoneRecordsToCsv.test.ts
@@ -106,8 +106,8 @@ sample4"
     ).toBe(expectedCsv);
   });
   it("should convert kintone records to csv string correctly when SUBTABLE value is an empty array", () => {
-    const expectedCsv = `"*","recordNumber","updatedTime","dropDown","creator","subTable","subTableText","subTableCheckbox","subTableFile","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
-*,"9","2021-02-16T02:43:00Z","sample1","username",,,,,"username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
+    const expectedCsv = `"*","recordNumber","updatedTime","dropDown","creator","subTable","subTableText","subTableCheckbox","subTableFile","userSelect","organizationSelect","groupSelect","modifier","richText","singleLineText","number","radioButton","multiLineText","createdTime","checkBox","calc","multiSelect"
+*,"9","2021-02-16T02:43:00Z","sample1","username",,,,,,,,"username","<div><div>rich text editor<br /></div></div><div>rich text editor<br /></div><div>rich text editor<br /></div>","""single line text""","8","sample2","multi
 line
 text","2021-02-10T06:14:00Z","""sample2""","16","""sample3""
 sample4"
