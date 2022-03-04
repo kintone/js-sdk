7a3d508aa8ce0f6d0f49b8253a2a46f69ae2da5a
ksm-cy <yczhou@cybozu.cn>
2022-03-04 16:49:37 +0900

    test(data-loader): add test for upload, fix fixture

--- packages/data-loader/src/kintone/__tests__/upload.test.ts
+++ packages/data-loader/src/kintone/__tests__/upload.test.ts
@@ -1,6 +1,10 @@
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 import { uploadRecords } from "../upload";
 
+import path from "path";
+
+import * as canUploadFiles from "./fixtures/can_upload_files";
+
 describe("import", () => {
   let apiClient: KintoneRestAPIClient;
   beforeEach(() => {
@@ -9,6 +13,7 @@ describe("import", () => {
       auth: { apiToken: "dummy" },
     });
   });
+
   it("should not fail", () => {
     apiClient.app.getFormFields = jest
       .fn()
@@ -41,6 +46,53 @@ describe("import", () => {
     expect(addRecordsMockFn.mock.calls[0][0]).toStrictEqual({
       app: APP_ID,
       records: RECORDS,
+    });
+  });
+
+  it("should upload files correctly when attachmentsDir is given", async () => {
+    const getFormFieldsMockFn = jest
+      .fn()
+      .mockResolvedValue({ properties: canUploadFiles.properties });
+    apiClient.app.getFormFields = getFormFieldsMockFn;
+    const uploadFileMockFn = jest
+      .fn()
+      .mockReturnValueOnce({ fileKey: "abcde" })
+      .mockReturnValueOnce({ fileKey: "fghij" });
+    apiClient.file.uploadFile = uploadFileMockFn;
+    const addRecordsMockFn = jest.fn().mockResolvedValue([{}]);
+    apiClient.record.addRecords = addRecordsMockFn;
+
+    const ATTACHMENTS_DIR = "AttachmentsFolder";
+    const APP_ID = "1";
+    const INPUT_RECORDS = canUploadFiles.input;
+    const EXPECTED_RECORDS = canUploadFiles.expected;
+
+    await uploadRecords({
+      apiClient,
+      attachmentsDir: ATTACHMENTS_DIR,
+      app: APP_ID,
+      records: INPUT_RECORDS,
+    });
+
+    // apiClient.file.uploadFile should be called with correct filePath
+    const fileInfos = canUploadFiles.input[0].attachment.value as Array<{
+      localFilePath: string;
+    }>;
+    expect(uploadFileMockFn.mock.calls[0][0]).toStrictEqual({
+      file: {
+        path: path.join(ATTACHMENTS_DIR, fileInfos[0].localFilePath),
+      },
+    });
+    expect(uploadFileMockFn.mock.calls[1][0]).toStrictEqual({
+      file: {
+        path: path.join(ATTACHMENTS_DIR, fileInfos[1].localFilePath),
+      },
+    });
+
+    // records should contain fileKeys
+    expect(addRecordsMockFn.mock.calls[0][0]).toStrictEqual({
+      app: APP_ID,
+      records: EXPECTED_RECORDS,
     });
   });
 
