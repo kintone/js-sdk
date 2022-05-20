b61edcd1e54bc940efb239ece5c4b83cdfc943ef
tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>
2022-05-20 14:51:51 +0900

    (subtree) feat(create-plugin): apply ESLint to generated project files (#1514)
    * test(create-plugin): check if created project passes lint check
    
    * chore(create-plugin): fix lint warning on modern template
    
    * fix(create-plugin): fix template

--- templates/modern/src/js/mobile.ts
+++ templates/modern/src/js/mobile.ts
@@ -7,7 +7,10 @@ const PLUGIN_ID = kintone.$PLUGIN_ID;
 kintone.events.on("mobile.app.record.index.show", () => {
   const config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-  const spaceElement = kintone.mobile.app.getHeaderSpaceElement()!;
+  const spaceElement = kintone.mobile.app.getHeaderSpaceElement();
+  if (spaceElement === null) {
+    throw new Error("The header element is unavailable on this page");
+  }
   const fragment = document.createDocumentFragment();
   const headingEl = document.createElement("h3");
   const messageEl = document.createElement("p");
