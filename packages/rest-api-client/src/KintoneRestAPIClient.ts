be67840afbe541db2d6aa015e9e4ec07776103c6
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-06-17 12:21:27 +0900

    refactor(rest-api-client): add validateBaseUrl()

--- packages/rest-api-client/src/KintoneRestAPIClient.ts
+++ packages/rest-api-client/src/KintoneRestAPIClient.ts
@@ -71,10 +71,6 @@ export class KintoneRestAPIClient {
     this.baseUrl = platformDeps
       .buildBaseUrl(options.baseUrl)
       .replace(/\/+$/, ""); // Remove trailing slash
-    const url = new URL(this.baseUrl);
-    if (url.hostname !== "localhost" && url.protocol !== "https:") {
-      throw new Error('The protocol of baseUrl must be "https".');
-    }
 
     const auth = buildDiscriminatedAuth(options.auth ?? {});
     const requestConfigBuilder = new KintoneRequestConfigBuilder({
@@ -125,7 +121,19 @@ export class KintoneRestAPIClient {
 }
 
 const validateOptions = (options: Options) => {
+  validateBaseUrl(options.baseUrl);
   validateGuestSpaceId(options.guestSpaceId);
+};
+
+const validateBaseUrl = (baseUrl: Options["baseUrl"]) => {
+  if (baseUrl === undefined) {
+    return;
+  }
+
+  const url = new URL(baseUrl);
+  if (url.hostname !== "localhost" && url.protocol !== "https:") {
+    throw new Error('The protocol of baseUrl must be "https".');
+  }
 };
 
 const validateGuestSpaceId = (guestSpaceId: Options["guestSpaceId"]) => {
