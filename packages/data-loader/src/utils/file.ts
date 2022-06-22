1f5596770a73495a3ada12a7abf589946aeebdde
kjmnmt <46946976+kjmnmt@users.noreply.github.com>
2022-06-22 13:54:14 +0900

    feat(data-loader): support specify character encoding when import (#1458)
    * feat(data-loader): support specify character encoding when import
    
    * refactor(data-loader): change SupportedImportEncoding type name
    
    * chore(data-loader): update yarn.lock
    
    * test(data-loader): add tests for readFile
    
    * docs(data-loader): update README.md
    
    * ci: disable autocrlf on test workflow
    
    * fix(data-loader): throw Error if format==json && encoding!=utf8
    
    Co-authored-by: Masaharu TASHIRO <masatsr.kit@gmail.com>
    Co-authored-by: tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>

--- packages/data-loader/src/utils/file.ts
+++ packages/data-loader/src/utils/file.ts
@@ -1,20 +1,30 @@
 import fs from "fs";
 import path from "path";
+import iconv from "iconv-lite";
+
+export type SupportedImportEncoding = "utf8" | "sjis";
 
 export const readFile: (
-  filePath: string
-) => Promise<{ content: string; format: string }> = async (filePath) => {
-  const stream = fs.createReadStream(filePath);
-  const content = await readStream(stream);
+  filePath: string,
+  encoding?: SupportedImportEncoding
+) => Promise<{ content: string; format: string }> = async (
+  filePath,
+  encoding = "utf8"
+) => {
   const format = extractFileFormat(filePath);
+  if (format === "json" && encoding !== "utf8") {
+    throw new Error("source file is JSON and JSON MUST be encoded with UTF-8");
+  }
+  const stream = fs
+    .createReadStream(filePath)
+    .pipe(iconv.decodeStream(encoding));
+  const content = await readStream(stream);
   return { content, format };
 };
 
-const readStream: (
-  stream: fs.ReadStream,
-  encoding?: string
-) => Promise<string> = async (stream, encoding = "utf8") => {
-  stream.setEncoding(encoding);
+const readStream: (stream: NodeJS.ReadWriteStream) => Promise<string> = async (
+  stream
+) => {
   let content = "";
   for await (const chunk of stream) {
     content += chunk;
