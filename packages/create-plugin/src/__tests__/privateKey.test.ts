eb181a0e61931b4b1981001e32f3a003f1ec5bdd
tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>
2022-06-23 18:33:43 +0900

    test(create-plugin): refactor test
    Co-authored-by: Shin'ya Ueoka <ueokande@i-beam.org>

--- packages/create-plugin/src/__tests__/privateKey.test.ts
+++ packages/create-plugin/src/__tests__/privateKey.test.ts
@@ -5,7 +5,7 @@ describe("privateKey", () => {
   describe("generatePrivateKey", () => {
     it("should be able to create a private key", () => {
       const key = generatePrivateKey();
-      assert(key.indexOf("-----BEGIN RSA PRIVATE KEY-----") === 0);
+      assert(key.startsWith("-----BEGIN RSA PRIVATE KEY-----"));
     });
   });
 });
