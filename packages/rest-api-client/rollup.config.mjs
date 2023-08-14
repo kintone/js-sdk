da8ace19aa6ba75be09ffecbcabc6ac131e09105
Nguyen Thai Hung <59815499+hung-cybo@users.noreply.github.com>
2023-08-14 13:41:19 +0900

    build: replace deprecated packages (#2221)

--- packages/rest-api-client/rollup.config.mjs
+++ packages/rest-api-client/rollup.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "rollup";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 import license from "rollup-plugin-license";
-import builtins from "rollup-plugin-node-builtins";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import globals from "rollup-plugin-node-globals";
 import babel from "@rollup/plugin-babel";
 import { ecmaVersionValidator } from "rollup-plugin-ecma-version-validator";
@@ -81,7 +81,7 @@ export default defineConfig({
     }),
     json(),
     globals(),
-    builtins(),
+    nodePolyfills(),
     isProd && terser(),
     ecmaVersionValidator({ ecmaVersion: 6 }),
     license({
