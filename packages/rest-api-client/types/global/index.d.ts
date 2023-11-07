fafeb2a8b56ef15bcf7e331e12fe495f50610a4e
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-11-07 17:12:58 +0900

    refactor `.d.ts` file

--- packages/rest-api-client/types/global/index.d.ts
+++ packages/rest-api-client/types/global/index.d.ts
@@ -1,21 +1,19 @@
 /* eslint-disable no-var,vars-on-top */
-declare namespace globalThis {
-  // We cannot use let,const to declare global object:(
-  // https://stackoverflow.com/a/69230938
-  var kintone: {
-    getRequestToken(): string;
-  };
+// We cannot use let,const to declare global object:(
+// https://stackoverflow.com/a/69230938
+declare var kintone: {
+  getRequestToken(): string;
+};
 
-  var garoon: {
-    connect: {
-      kintone: {
-        getRequestToken(): Promise<string>;
-      };
+declare var garoon: {
+  connect: {
+    kintone: {
+      getRequestToken(): Promise<string>;
     };
   };
+};
 
-  // see https://github.com/DefinitelyTyped/DefinitelyTyped/issues/24419
-  declare interface Element {}
-  declare interface Document {}
-  declare interface NodeListOf<T = {}> {}
-}
+// see https://github.com/DefinitelyTyped/DefinitelyTyped/issues/24419
+declare interface Element {}
+declare interface Document {}
+declare interface NodeListOf<T = {}> {}
