2260a5d1bcadb9c43c0dc24773d1c98e289f7ac2
hung-nguyen <hung-nguyen@cybozu.vn>
2024-01-11 22:00:45 +0700

    test: upgrade Vite and add both test for cli and api

--- packages/rest-api-client/src/__tests__/e2e.vite.api.test.ts
+++ packages/rest-api-client/src/__tests__/e2e.vite.api.test.ts
@@ -1,4 +1,5 @@
 import assert from "assert";
+import type { BuildOptions } from "vite";
 import { build } from "vite";
 import path from "path";
 import fs from "fs";
@@ -7,19 +8,29 @@ import os from "os";
 
 const TESTCASE_TIMEOUT = 30000;
 
-export const tempDir = fs.mkdtempSync(
+const tempDir = fs.mkdtempSync(
   path.join(os.tmpdir(), "kintone-rest-api-client-vite-bundle-"),
 );
 
-describe.skip("Vite Bundler tests", function () {
+describe("Vite API Bundler tests", function () {
   it(
     `should be able to build with Vite successfully`,
     async () => {
       try {
+        const buildConfig: BuildOptions = {
+          lib: {
+            entry: path.resolve(__dirname, "./fixtures/index.ts"),
+            formats: ["umd"],
+            name: "MyBundle",
+            fileName: "bundle.vite",
+          },
+          outDir: path.resolve(tempDir, "dist"),
+          emptyOutDir: true,
+        };
         await build({
-          logLevel: "info",
           envFile: false,
-          configFile: path.resolve(__dirname, "fixtures/vite.config.ts"),
+          configFile: false,
+          build: buildConfig,
         });
         assert.ok(
           fs.existsSync(path.resolve(tempDir, "dist", "bundle.vite.umd.js")),
---
+++ packages/rest-api-client/src/__tests__/e2e.vite.cli.test.ts
@@ -0,0 +1,32 @@
+import { spawnSync } from "child_process";
+import fs from "fs";
+import path from "path";
+import os from "os";
+import { rimrafSync } from "rimraf";
+
+const tempDir = fs.mkdtempSync(
+  path.join(os.tmpdir(), "kintone-rest-api-client-bundle-"),
+);
+
+describe("Vite CLI Bundler tests", function () {
+  it(`should be able to build with Vite successfully`, async () => {
+    const buildResult = spawnSync(
+      "vite build",
+      ["--config", "fixtures/vite.config.mjs"],
+      {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true,
+        env: {
+          ...process.env,
+          TEMP_DIR: tempDir,
+        },
+      },
+    );
+    expect(buildResult.status).toBe(0);
+  });
+
+  afterAll(() => {
+    rimrafSync(tempDir);
+  });
+});
