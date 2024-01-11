d4c422f2b353a353a45511d1130fa3f5983c183f
hung-nguyen <hung-nguyen@cybozu.vn>
2024-01-11 16:39:55 +0700

    test: clean code

--- packages/rest-api-client/src/__tests__/e2e.vite.test.ts
+++ packages/rest-api-client/src/__tests__/e2e.vite.test.ts
@@ -1,15 +1,9 @@
 import assert from "assert";
-// import type { BuildOptions } from "vite";
 import { build } from "vite";
 import path from "path";
 import fs from "fs";
-// import os from "os";
 import { rimrafSync } from "rimraf";
 import { tempDir } from "./fixtures/vite.config";
-
-// const tempDir = fs.mkdtempSync(
-//   path.join(os.tmpdir(), "kintone-rest-api-client-vite-bundle-"),
-// );
 
 const TESTCASE_TIMEOUT = 30000;
 
@@ -17,23 +11,11 @@ describe("Vite Bundler tests", function () {
   it(
     `should be able to build with Vite successfully`,
     async () => {
-      // const buildConfig: BuildOptions = {
-      //   lib: {
-      //     entry: path.resolve(__dirname, "fixtures/index.ts"),
-      //     fileName: "bundle",
-      //     formats: ["umd"],
-      //     name: "MyBundle",
-      //   },
-      //   outDir: path.resolve(tempDir, "dist"),
-      // };
-
       try {
         await build({
           logLevel: "info",
           envFile: false,
           configFile: path.resolve(__dirname, "fixtures/vite.config.ts"),
-          // configFile: false,
-          // build: buildConfig,
         });
         assert.ok(
           fs.existsSync(path.resolve(tempDir, "dist", "bundle.vite.umd.js")),
