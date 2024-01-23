6c3cb93c5d3f0c303034f20fb4587999a23abe72
Tuan Pham <tuan-pham@cybozu.vn>
2024-01-23 10:47:50 +0700

    chore!(create-plugin): refactor code

--- packages/create-plugin/templates/minimum/src/js/config.js
+++ packages/create-plugin/templates/minimum/src/js/config.js
@@ -1,27 +1,25 @@
 (function (PLUGIN_ID) {
   "use strict";
 
-  var $form = document.querySelectorAll(".js-submit-settings");
-  var $cancelButton = document.querySelectorAll(".js-cancel-button");
-  var $message = document.querySelectorAll(".js-text-message");
-  if (!($form.length > 0 && $cancelButton.length > 0 && $message.length > 0)) {
+  var $form = document.querySelector(".js-submit-settings");
+  var $cancelButton = document.querySelector(".js-cancel-button");
+  var $message = document.querySelector(".js-text-message");
+  if (!($form && $cancelButton && $message)) {
     throw new Error("Required elements do not exist.");
   }
   var config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
   if (config.message) {
-    $message[0].value = config.message;
+    $message.value = config.message;
   }
-  $form[0].addEventListener("submit", function (e) {
+  $form.addEventListener("submit", function (e) {
     e.preventDefault();
-    kintone.plugin.app.setConfig({ message: $message[0].value }, function () {
+    kintone.plugin.app.setConfig({ message: $message.value }, function () {
       alert("The plug-in settings have been saved. Please update the app!");
       window.location.href = "../../flow?app=" + kintone.app.getId();
     });
   });
-  $cancelButton.forEach((element) => {
-    element.addEventListener("click", function () {
-      window.location.href = "../../" + kintone.app.getId() + "/plugin/";
-    });
+  $cancelButton.addEventListener("click", function () {
+    window.location.href = "../../" + kintone.app.getId() + "/plugin/";
   });
 })(kintone.$PLUGIN_ID);
