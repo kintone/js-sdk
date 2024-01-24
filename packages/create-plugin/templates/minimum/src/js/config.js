9fcf529e07cce7eb0c2e94100621c3d95241cab4
Tuan Pham <tuan-pham@cybozu.vn>
2024-01-24 08:04:52 +0700

    chore!(create-plugin): modify variable name, use  instead of

--- packages/create-plugin/templates/minimum/src/js/config.js
+++ packages/create-plugin/templates/minimum/src/js/config.js
@@ -1,25 +1,29 @@
 (function (PLUGIN_ID) {
   "use strict";
 
-  var $form = document.querySelector(".js-submit-settings");
-  var $cancelButton = document.querySelector(".js-cancel-button");
-  var $message = document.querySelector(".js-text-message");
-  if (!($form && $cancelButton && $message)) {
+  let formEl = document.querySelector(".js-submit-settings");
+  let cancelButtonEl = document.querySelector(".js-cancel-button");
+  let messageEl = document.querySelector(".js-text-message");
+  if (
+    !(formEl.length > 0 && cancelButtonEl.length > 0 && messageEl.length > 0)
+  ) {
     throw new Error("Required elements do not exist.");
   }
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
   if (config.message) {
-    $message.value = config.message;
+    messageEl.value = config.message;
   }
-  $form.addEventListener("submit", function (e) {
+
+  formEl.addEventListener("submit", function (e) {
     e.preventDefault();
-    kintone.plugin.app.setConfig({ message: $message.value }, function () {
+    kintone.plugin.app.setConfig({ message: messageEl.value }, function () {
       alert("The plug-in settings have been saved. Please update the app!");
       window.location.href = "../../flow?app=" + kintone.app.getId();
     });
   });
-  $cancelButton.addEventListener("click", function () {
+
+  cancelButtonEl.addEventListener("click", function () {
     window.location.href = "../../" + kintone.app.getId() + "/plugin/";
   });
 })(kintone.$PLUGIN_ID);
--- packages/create-plugin/templates/minimum/src/js/desktop.js
+++ packages/create-plugin/templates/minimum/src/js/desktop.js
@@ -2,23 +2,23 @@
   "use strict";
 
   kintone.events.on("app.record.index.show", function () {
-    var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-
-    var spaceElement = kintone.app.getHeaderSpaceElement();
-    if (spaceElement === null) {
-      throw new Error("The header element is unavailable on this page");
+    let spaceEl = kintone.app.getHeaderSpaceElement();
+    if (spaceEl === null) {
+      throw new Error("The header element is unavailable on this page.");
     }
-    var fragment = document.createDocumentFragment();
-    var headingEl = document.createElement("h3");
-    var messageEl = document.createElement("p");
 
-    messageEl.classList.add("plugin-space-message");
+    let fragment = document.createDocumentFragment();
+    let headingEl = document.createElement("h3");
+    let messageEl = document.createElement("p");
+
+    const config = kintone.plugin.app.getConfig(PLUGIN_ID);
     messageEl.textContent = config.message;
-    headingEl.classList.add("plugin-space-heading");
+    messageEl.classList.add("plugin-space-message");
     headingEl.textContent = "Hello kintone plugin!";
+    headingEl.classList.add("plugin-space-heading");
 
     fragment.appendChild(headingEl);
     fragment.appendChild(messageEl);
-    spaceElement.appendChild(fragment);
+    spaceEl.appendChild(fragment);
   });
 })(kintone.$PLUGIN_ID);
