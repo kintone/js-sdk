d8b46336c5b2a77fc0ca543b51fd5758193bc768
hung-nguyen <hung-nguyen@cybozu.vn>
2024-02-05 15:44:45 +0700

    chore: migrate to @cybozu/license-manager

---
+++ license-manager.config.js
@@ -0,0 +1,31 @@
+const config = {
+  analyze: {
+    allowLicenses: [
+      "MIT",
+      "Apache-2.0",
+      "BSD-2-Clause",
+      "BSD-3-Clause",
+      "BSD-3-Clause OR MIT",
+      "ISC",
+      "0BSD",
+      "Python-2.0",
+      "MPL-2.0",
+      "CC0-1.0",
+      "CC-BY-3.0",
+      "CC-BY-4.0",
+      "(MIT OR CC0-1.0)",
+      "(MIT AND Zlib)",
+      "(MIT AND BSD-3-Clause)",
+      "(MIT AND CC-BY-3.0)",
+      "BlueOak-1.0.0",
+      "(BSD-3-Clause OR GPL-2.0)",
+      "Unlicense",
+    ],
+    allowPackages: [
+      "indexof",
+    ],
+  },
+  packageManager: "pnpm",
+};
+
+module.exports = config;
