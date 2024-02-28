d33bdbe8b744d3f7a5725fe61c204bda1a07603e
Tuan Pham <tuan-pham@cybozu.vn>
2024-02-28 17:13:48 +0700

    feat(rest-api-client): add unit test

--- packages/rest-api-client/src/client/__tests__/SpaceClient.test.ts
+++ packages/rest-api-client/src/client/__tests__/SpaceClient.test.ts
@@ -53,6 +53,25 @@ describe("SpaceClient", () => {
       expect(mockClient.getLogs()[0].params).toEqual(params);
     });
   });
+
+  describe("updateSpaceBody", () => {
+    const params = {
+      id: SPACE_ID,
+      body: "<b>This is a space body</b>",
+    };
+    beforeEach(async () => {
+      await spaceClient.updateSpaceBody(params);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe("/k/v1/space/body.json");
+    });
+    it("should send a delete request", () => {
+      expect(mockClient.getLogs()[0].method).toBe("put");
+    });
+    it("should pass id, body to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual(params);
+    });
+  });
 });
 
 describe("SpaceClient with guestSpaceId", () => {
