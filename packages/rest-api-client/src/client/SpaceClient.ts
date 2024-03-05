52b73725a4daea0addcdb981442d550126f7b476
Tuan Pham <tuan-pham@cybozu.vn>
2024-03-05 09:57:42 +0700

    remove redundant code

--- packages/rest-api-client/src/client/SpaceClient.ts
+++ packages/rest-api-client/src/client/SpaceClient.ts
@@ -16,13 +16,6 @@ export class SpaceClient extends BaseClient {
     return this.client.delete(path, params);
   }
 
-  public updateSpaceBody(params: { id: SpaceID; body: string }): Promise<{}> {
-    const path = this.buildPathWithGuestSpaceId({
-      endpointName: "space/body",
-    });
-    return this.client.put(path, params);
-  }
-
   public getSpaceMembers(params: { id: SpaceID }): Promise<SpaceMembers> {
     const path = this.buildPathWithGuestSpaceId({
       endpointName: "space/members",
