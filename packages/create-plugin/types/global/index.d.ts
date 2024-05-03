48f51dd2f4d1e01416f873f9b8aa26bdfcea74ff
Nguyen Thai Hung <59815499+hung-cybo@users.noreply.github.com>
2024-05-03 14:48:18 +0700

    test(create-plugin): adding teardown process for e2e test (#2735)
    Co-authored-by: Tuan Pham <tuan-pham@cybozu.vn>

--- packages/create-plugin/types/global/index.d.ts
+++ packages/create-plugin/types/global/index.d.ts
@@ -2,3 +2,10 @@
 declare interface Element {}
 declare interface Document {}
 declare interface NodeListOf<T = {}> {}
+
+/* eslint-disable no-var,vars-on-top */
+// We cannot use let,const to declare global object:(
+// https://stackoverflow.com/a/69230938
+declare global {
+  var testStatuses: { [testPath: string]: "failed" | "passed" | "running" };
+}
