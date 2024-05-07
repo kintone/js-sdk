a614553a471acf55739cd4be6812135dd932f860
Nguyen Thai Hung <59815499+hung-cybo@users.noreply.github.com>
2024-05-07 13:02:14 +0700

    (subtree) test(create-plugin): add test cases for JsSdkTest-10,11,12,13 (#2742)

---
+++ __e2e__/fixtures/pluginDescriptionContain200Chars.ts
@@ -0,0 +1,58 @@
+import type { TestPattern } from "../e2e.test";
+import {
+  ANSWER_NO,
+  CREATE_PLUGIN_COMMAND,
+  DEFAULT_ANSWER,
+} from "../utils/constants";
+import { getBoundMessage } from "../../src/messages";
+import { generateRandomString } from "../utils/helper";
+
+const m = getBoundMessage("en");
+const pluginDescription = generateRandomString(200);
+
+export const pattern: TestPattern = {
+  description:
+    "#JsSdkTest-3 Should able to create a plugin with plugin-in description contains 200 characters",
+  input: {
+    command: CREATE_PLUGIN_COMMAND,
+    outputDir: "test3",
+    questionsInput: [
+      {
+        question: m("Q_NameEn"),
+        answer: "200characters",
+      },
+      {
+        question: m("Q_DescriptionEn"),
+        answer: pluginDescription,
+      },
+      {
+        question: m("Q_SupportJa"),
+        answer: ANSWER_NO,
+      },
+      {
+        question: m("Q_SupportZh"),
+        answer: ANSWER_NO,
+      },
+      {
+        question: m("Q_WebsiteUrlEn"),
+        answer: DEFAULT_ANSWER,
+      },
+      {
+        question: m("Q_MobileSupport"),
+        answer: ANSWER_NO,
+      },
+      {
+        question: m("Q_EnablePluginUploader"),
+        answer: ANSWER_NO,
+      },
+    ],
+  },
+  expected: {
+    success: {
+      manifestJson: {
+        name: { en: "200characters" },
+        description: { en: pluginDescription },
+      },
+    },
+  },
+};
--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -3,6 +3,7 @@ const config = {
   roots: ["<rootDir>"],
   testRegex: "/__e2e__/.*\\.test\\.ts$",
   testEnvironment: "./JestCustomEnvironment.js",
+  setupFilesAfterEnv: ["<rootDir>/__e2e__/setup.ts"],
   testTimeout: 120000,
 };
 module.exports = config;
