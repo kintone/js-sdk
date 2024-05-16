3aca1f68669d65b56a462f7c4c6e256484bad6fa
Tuan Pham <tuan-pham@cybozu.vn>
2024-05-16 13:55:31 +0700

    build: no need tmp as current code

--- packages/dts-gen/bin/npm-build.js
+++ packages/dts-gen/bin/npm-build.js
@@ -3,10 +3,8 @@ const path = require("path");
 
 const sourceFile = path.join(__dirname, "..", "dist", "index.js");
 const shebangFile = path.join(__dirname, "..", "resources", "shebang.txt");
-const tmpFile = path.join(__dirname, "..", "dist", "index.js.tmp");
 
 const shebangContent = fs.readFileSync(shebangFile, "utf8");
 const originalContent = fs.readFileSync(sourceFile, "utf8");
 
-fs.writeFileSync(tmpFile, shebangContent + originalContent);
-fs.renameSync(tmpFile, sourceFile);
+fs.writeFileSync(sourceFile, shebangContent + originalContent);
