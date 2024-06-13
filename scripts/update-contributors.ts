72ba237d4798f6ac62d7103c3e9600a95e601955
Masaharu Tashiro <tasshi.me@gmail.com>
2024-06-14 07:51:38 +0900

    chore: fix calculation errors

--- scripts/update-contributors.ts
+++ scripts/update-contributors.ts
@@ -15,20 +15,21 @@ const file = "README.md";
   const octokit = new Octokit();
 
   // We use stats because the response of octokit.repos.listContributors is missing some contributors.
-  const resp = await octokit.paginate(
-    "GET /repos/{owner}/{repo}/contributors",
-    {
-      owner: "kintone",
-      repo: "js-sdk",
-    },
-  );
+  const resp = await octokit.repos.getContributorsStats({
+    owner: "kintone",
+    repo: "js-sdk",
+  });
 
-  const contributors = resp
-    .filter((c) => c.type === "User" && !ignoredUsers.includes(c.login ?? ""))
+  const contributors = resp.data
+    .filter(
+      (c) =>
+        c.author?.type === "User" && !ignoredUsers.includes(c.author.login),
+    )
+    .sort((c1, c2) => c2.total - c1.total)
     .map((c) => ({
-      login: c.login,
-      avatar_url: c.avatar_url,
-      html_url: c.html_url,
+      login: c.author?.login,
+      avatar_url: c.author?.avatar_url,
+      html_url: c.author?.html_url,
     }));
 
   const contributorIcons = contributors
