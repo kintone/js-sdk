d7a04492f6fcfd8c8bfaed21c4e5b9767bbd40aa
shabaraba <takahiko-kawakami@cybozu.co.jp>
2024-07-05 20:30:39 +0900

    feat(rest-api-client): Lang type split into AppLang and PluginLocale

--- packages/rest-api-client/src/client/AppClient.ts
+++ packages/rest-api-client/src/client/AppClient.ts
@@ -3,7 +3,7 @@ import type {
   RecordID,
   Revision,
   Properties,
-  Lang,
+  AppLang,
   Layout,
   ViewForResponse,
   ViewForParameter,
@@ -33,6 +33,7 @@ import type {
   ReportForResponse,
   AppActionsForParameter,
   AppActionsForResponse,
+  PluginLocale,
 } from "./types";
 import { BaseClient } from "./BaseClient";
 import type { AppPlugin } from "./types/app/plugin";
@@ -69,7 +70,7 @@ type SpaceResponseParameters = {
 export class AppClient extends BaseClient {
   public getFormFields<T extends Properties>(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{ properties: T; revision: string }> {
     const { preview, ...rest } = params;
@@ -142,7 +143,7 @@ export class AppClient extends BaseClient {
 
   public getViews(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     views: { [viewName: string]: ViewForResponse };
@@ -222,7 +223,7 @@ export class AppClient extends BaseClient {
 
   public getAppSettings(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     name: string;
@@ -293,7 +294,7 @@ export class AppClient extends BaseClient {
 
   public getProcessManagement(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     enable: boolean;
@@ -414,7 +415,7 @@ export class AppClient extends BaseClient {
 
   public getRecordAcl(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{ rights: RecordRightForResponse[]; revision: string }> {
     const { preview, ...rest } = params;
@@ -500,7 +501,7 @@ export class AppClient extends BaseClient {
 
   public getPerRecordNotifications(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     notifications: PerRecordNotificationForResponse[];
@@ -528,7 +529,7 @@ export class AppClient extends BaseClient {
 
   public getReminderNotifications(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     notifications: ReminderNotificationForResponse[];
@@ -558,7 +559,7 @@ export class AppClient extends BaseClient {
 
   public getReports(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     reports: { [reportName: string]: ReportForResponse };
@@ -589,7 +590,7 @@ export class AppClient extends BaseClient {
 
   public getAppActions(params: {
     app: AppID;
-    lang?: Lang;
+    lang?: AppLang;
     preview?: boolean;
   }): Promise<{
     actions: AppActionsForResponse;
@@ -620,7 +621,7 @@ export class AppClient extends BaseClient {
 
   public getPlugins(params: {
     app: AppID;
-    lang?: Exclude<Lang, "default" | "user">;
+    lang?: PluginLocale;
     preview?: boolean;
   }): Promise<{
     plugins: AppPlugin[];
