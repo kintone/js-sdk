4e02410d46d3fc3a4480879dbce600172bf9a78a
Takahiko Kawakami <53943902+shabaraba@users.noreply.github.com>
2024-08-20 15:29:48 +0900

    refactor(create-plugin): refactor for inquirer update (#2924)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- packages/customize-uploader/src/params/init.ts
+++ packages/customize-uploader/src/params/init.ts
@@ -1,27 +1,10 @@
-import * as inquirer from "inquirer";
 import type { Lang } from "../lang";
 import { getBoundMessage } from "../messages";
+import { promptForAppId, promptForScope } from "./prompts/init";
 
-export const inquireInitParams = (lang: Lang) => {
-  const appId: string = "";
-  const scope: string = "";
+export const inquireInitParams = async (lang: Lang) => {
   const m = getBoundMessage(lang);
-  const questions = [
-    {
-      type: "input",
-      message: m("Q_AppId"),
-      name: "appId",
-      validate: (v: string) => !!v,
-    },
-    {
-      type: "list",
-      message: m("Q_Scope"),
-      name: "scope",
-      choices: ["ALL", "ADMIN", "NONE"],
-    },
-  ];
-
-  return inquirer
-    .prompt(questions)
-    .then((answers) => Object.assign({ appId, scope, lang }, answers));
+  const appId = await promptForAppId(m);
+  const scope = await promptForScope(m);
+  return { appId, scope, lang };
 };
