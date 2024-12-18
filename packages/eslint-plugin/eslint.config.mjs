1d8fb61ff4085a1b8720681ce579d64ae96f2dff
Masaharu TASHIRO <tasshi.me@gmail.com>
2024-12-18 14:42:25 +0900

    lint: fix lint config

--- packages/eslint-plugin/eslint.config.mjs
+++ packages/eslint-plugin/eslint.config.mjs
@@ -1,4 +1,4 @@
 import rootConfig from "../../eslint.config.mjs";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...rootConfig];
+export default [{ ignores: ["**/lib"] }, ...rootConfig];
