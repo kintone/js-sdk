48b860d4f43d5caf81d791763498b9fe3618c0b9
shabaraba <takahiko-kawakami@cybozu.co.jp>
2025-02-10 11:55:10 +0900

    chore(rest): remove esm and lib from git management

--- packages/rest/esm/src/index.js
+++
@@ -1,4 +0,0 @@
-import createClient from "openapi-fetch";
-import { iterator } from "./helpers/iterator";
-export { createClient, iterator };
-//# sourceMappingURL=index.js.map
--- packages/rest/lib/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import createClient from "openapi-fetch";
-import { paths } from "./schemas/schema";
-import { iterator } from "./helpers/iterator";
-export { createClient, iterator, paths };
