d68c04193170e23f1ef478fd309db0d8dd5997e0
shabaraba <takahiko-kawakami@cybozu.co.jp>
2025-02-26 17:23:10 +0900

    feat(rest): add kintone.api interface

--- packages/rest/src/client/KintoneClient/index.ts
+++ packages/rest/src/client/KintoneClient/index.ts
@@ -1,7 +1,30 @@
-import type { Client } from "openapi-fetch";
-import type { MediaType } from "openapi-typescript-helpers";
+import type { Client, MaybeOptionalInit, FetchResponse } from "openapi-fetch";
+import type { MediaType, HttpMethod } from "openapi-typescript-helpers";
 
 export interface KintoneClient<
   Paths extends {},
   Media extends MediaType = MediaType,
-> extends Client<Paths, Media> {}
+> extends Client<Paths, Media> {
+  api: KintoneClientMethod<Paths, Media>;
+}
+
+export type MethodWithPath<Path extends Record<string, any>> = {
+  [Method in keyof Path]: Path[Method] extends never | undefined
+    ? never
+    : Method extends "get" | "post" | "put" | "delete"
+      ? Method
+      : never;
+}[keyof Path];
+
+export type KintoneClientMethod<
+  Paths extends Record<string, Record<HttpMethod, {}>>,
+  Media extends MediaType,
+> = <
+  Path extends keyof Paths,
+  Method extends MethodWithPath<Paths[Path]>,
+  Init extends MaybeOptionalInit<Paths[Path], Method>,
+>(
+  url: Path,
+  method: Method,
+  body: Init & { [key: string]: unknown },
+) => Promise<FetchResponse<Paths[Path][Method], Init, Media>>;
--- packages/rest/src/client/index.ts
+++ packages/rest/src/client/index.ts
@@ -1,22 +1,43 @@
-import _createClient from "openapi-fetch";
-import type { MediaType } from "openapi-typescript-helpers";
+import _createClient, {
+  FetchResponse,
+  MaybeOptionalInit,
+} from "openapi-fetch";
+import type {
+  HttpMethod,
+  MediaType,
+} from "openapi-typescript-helpers";
 import type { KintoneClientOptions } from "./KintoneClientOptions";
 import { buildNativeClientOptions } from "./KintoneClientOptions";
 import { getCsrfMiddleware } from "./Middlewares/CsrfMiddleware";
-import { KintoneClient } from "./KintoneClient";
+import { KintoneClient, MethodWithPath } from "./KintoneClient";
 import { isSessionAuth } from "./KintoneClientOptions/Auth";
 
 export const createClient = <
-  Paths extends {},
+  Paths extends Record<string, Record<HttpMethod, {}>>,
   Media extends MediaType = MediaType,
 >(
   clientOptions: KintoneClientOptions,
 ): KintoneClient<Paths, Media> => {
   const nativeClientOptions = buildNativeClientOptions(clientOptions);
-  const client = _createClient<Paths, Media>(nativeClientOptions);
+  const client = _createClient<Paths, Media>(
+    nativeClientOptions,
+  ) as KintoneClient<Paths, Media>;
 
   if (isSessionAuth(clientOptions.auth)) {
     client.use(getCsrfMiddleware());
   }
+
+  client.api = async <
+    Path extends keyof Paths,
+    Method extends MethodWithPath<Paths[Path]>,
+    Init extends MaybeOptionalInit<Paths[Path], Method>,
+  >(
+    url: any,
+    method: any,
+    body: any,
+  ): Promise<FetchResponse<Paths[Path][Method], Init, Media>> => {
+    return client.request(method, url, body);
+  };
+
   return client;
 };
