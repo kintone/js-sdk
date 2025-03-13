0282366e03142187d848f8ee8c02e9e2784c039c
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2025-03-13 10:59:38 +0900

    chore(rest): add new cap exceptions GET/POST/PUT/DELETE

--- packages/rest/eslint.config.mjs
+++ packages/rest/eslint.config.mjs
@@ -2,8 +2,16 @@ import rootConfig from "../../eslint.config.mjs";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
+  ...rootConfig,
   {
     ignores: ["lib", "esm", "src/schemas"],
+    rules: {
+      "new-cap": ["warn", {
+        "capIsNewExceptions": [
+          "Deferred", // Because it is defined in the inheritance source.
+          "GET", "POST", "PUT", "DELETE"
+        ]
+      }]
+    }
   },
-  ...rootConfig,
 ];
