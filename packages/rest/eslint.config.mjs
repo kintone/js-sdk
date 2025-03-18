12f9ff56dd17a3c17a194a0fa7075082ae45c88b
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-03-18 16:56:33 +0900

    feat(rest): (wip) build umd

--- packages/rest/eslint.config.mjs
+++ packages/rest/eslint.config.mjs
@@ -3,7 +3,7 @@ import rootConfig from "../../eslint.config.mjs";
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
-    ignores: ["lib", "esm", "src/schemas"],
+    ignores: ["lib", "esm", "umd", "src/schemas"],
   },
   ...rootConfig,
 ];
