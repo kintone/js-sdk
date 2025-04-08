4215d9ccdc1406e4014c4727ebde4fc1d265d439
Masaharu Tashiro <tasshi.me@gmail.com>
2025-04-09 06:47:26 +0900

    chore: refactor npm-scripts

--- packages/rest/vite.config.ts
+++ packages/rest/vite.config.ts
@@ -5,47 +5,51 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const outDir = "lib";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: [
-        path.resolve(__dirname, "src/index.ts"),
-        path.resolve(__dirname, "src/index.browser.ts"),
-      ],
-      formats: ["es", "cjs"],
-      fileName: "[format]/[name]",
-    },
-    sourcemap: "inline",
-    outDir: outDir,
-    rollupOptions: {
-      output: {
-        exports: "named",
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        entryFileNames: "[format]/[name].js",
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    build: {
+      lib: {
+        entry: [
+          path.resolve(__dirname, "src/index.ts"),
+          path.resolve(__dirname, "src/index.browser.ts"),
+        ],
+        formats: ["es", "cjs"],
+        fileName: "[format]/[name]",
       },
-      external: [/node_modules/],
+      sourcemap: isProd ? false : "inline",
+      outDir: outDir,
+      rollupOptions: {
+        output: {
+          exports: "named",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[format]/[name].js",
+        },
+        external: [/node_modules/],
+      },
     },
-  },
-  plugins: [
-    dts({
-      // tsconfigPath: "tsconfig.build.vite.json",
-      outDir: [path.join(outDir, "es"), path.join(outDir, "cjs")],
-      exclude: ["**/__tests__/*", "**/*.test.ts"],
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/package.esm.json",
-          dest: "es",
-          rename: "package.json",
-        },
-        {
-          src: "src/package.cjs.json",
-          dest: "cjs",
-          rename: "package.json",
-        },
-      ],
-    }),
-  ],
+    plugins: [
+      dts({
+        // tsconfigPath: "tsconfig.build.vite.json",
+        outDir: [path.join(outDir, "es"), path.join(outDir, "cjs")],
+        exclude: ["**/__tests__/*", "**/*.test.ts"],
+      }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "src/package.esm.json",
+            dest: "es",
+            rename: "package.json",
+          },
+          {
+            src: "src/package.cjs.json",
+            dest: "cjs",
+            rename: "package.json",
+          },
+        ],
+      }),
+    ],
+  };
 });
