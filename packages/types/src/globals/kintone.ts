47c9080b85b69904c50883b2aea3f6f54f3d1a09
Masaharu Tashiro <tasshi.me@gmail.com>
2025-09-29 18:41:22 +0900

    wip

--- packages/types/src/globals/kintone.ts
+++ packages/types/src/globals/kintone.ts
@@ -5,6 +5,7 @@ import type { Api } from "./api.js";
 import type { Proxy } from "./proxy.js";
 import type { MobilePortal, Portal } from "./portal.js";
 import type { MobileSpace, Space } from "./space.js";
+import type { Plugin } from "./plugin.js";
 
 export type Kintone = General & {
   events: Events;
@@ -13,6 +14,7 @@ export type Kintone = General & {
   proxy: Proxy;
   portal: Portal;
   space: Space;
+  plugin: Plugin;
   mobile: Mobile;
 };
 
