8a5da2744a71957e768d27b7b452249b584f00fd
shabaraba <work.shabaraba@gmail.com>
2025-12-24 10:41:26 +0900

    chore: create logger internal package

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,9 @@
+export { StandardLogger, logger } from "./logger.js";
+export {
+  LOG_CONFIG_LEVELS,
+  type Logger,
+  type LogConfigLevel,
+  type LogEvent,
+  type LogEventLevel,
+  type Printer,
+} from "./types.js";
