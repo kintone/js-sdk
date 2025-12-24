c9d48fae1ecb18a204ecadb55de16fb9239fbb80
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-12-24 18:00:30 +0900

    test: migrate jest to vitest

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  roots: ["<rootDir>/__tests__"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "lib/"],
-};
-module.exports = config;
