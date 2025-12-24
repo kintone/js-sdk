717fe58ef27c0851709adc6f2ecfc65ed6729375
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-12-24 14:28:56 +0900

    test(dts-gen): migrate jest to vitest

--- packages/dts-gen/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  roots: ["<rootDir>/src"],
-  testEnvironment: "node",
-  testTimeout: 30000,
-};
-module.exports = config;
