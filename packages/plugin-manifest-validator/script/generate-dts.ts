668db84bba0b5d1288d2967711c675f74ac0674b
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-12-25 16:10:19 +0900

    chore: use tsx to run ts scripts

--- packages/plugin-manifest-validator/script/generate-dts.ts
+++ packages/plugin-manifest-validator/script/generate-dts.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { compile } from "json-schema-to-typescript";
-import schema from "../manifest-schema.json" with { type: "json" };
+import schema from "../manifest-schema.json";
 
 const schemaWithoutAnyOf = schema as any;
 delete schemaWithoutAnyOf.definitions.resources.items.anyOf;
