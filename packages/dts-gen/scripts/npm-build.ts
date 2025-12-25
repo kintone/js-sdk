1d309b1c42fb953dd071fe4a3fe4fb33f5b706f1
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-12-25 14:51:27 +0900

    chore: update js scripts to ts

--- packages/dts-gen/scripts/npm-build.ts
+++ packages/dts-gen/scripts/npm-build.ts
@@ -1,5 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const sourceFile = path.join(__dirname, "..", "dist", "index.js");
 const shebangFile = path.join(__dirname, "..", "resources", "shebang.txt");
