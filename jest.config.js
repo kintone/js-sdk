32dae821656258813284771b98811a260896f4b7
Nameless <55537988+nameless-mc@users.noreply.github.com>
2026-01-09 17:56:13 +0900

    (subtree) test: migrate jest to vitest (#3541)

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  roots: ["<rootDir>/src"],
-  testRegex: "/__tests__/.*\\.test\\.ts$",
-  testEnvironment: "node",
-};
-module.exports = config;
