e675245e8180bc2fada6567a8e78d4428fbf6d98
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2026-01-09 12:04:21 +0900

    test(webpack-plugin-kintone-plugin): use built index.js in test

--- packages/webpack-plugin-kintone-plugin/vitest.config.ts
+++ packages/webpack-plugin-kintone-plugin/vitest.config.ts
@@ -1,6 +1,13 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Use built CommonJS file instead of ESM source to avoid module.exports issue
+      "../index": path.resolve(__dirname, "dist/index.js"),
+    },
+  },
   test: {
     globals: true,
     exclude: [
