312afd926e77432d00dce7c038dc0938c6cdb704
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2026-03-06 15:25:14 +0900

    fix(dts-gen): rm lodash deps

--- packages/dts-gen/src/utils/objectvalues.ts
+++ packages/dts-gen/src/utils/objectvalues.ts
@@ -1,5 +1,3 @@
-import { toPairs } from "lodash";
-
 export const objectValues = <T extends Record<string, any>>(object: T) => {
-  return toPairs(object).map((entry) => entry[1]);
+  return Object.values(object);
 };
